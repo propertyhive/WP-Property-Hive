6cc052a1bf4b54308f8645b7de56ffa22d2d215c
propertyhive <info@wp-property-hive.com>
2015-11-27 13:40:44 +0000

    Improve the way hiding and showing of department-based controls works

--- assets/js/frontend/search.js
+++ assets/js/frontend/search.js
@@ -1,39 +1,55 @@
-jQuery( function($){
-    
-    // Orderby
-    $( '.propertyhive-ordering' ).on( 'change', 'select.orderby', function() {
-        $( this ).closest( 'form' ).submit();
-    });
-    
-    toggleDepartmentFields();
-    
-    $('input[type=\'radio\'][name=\'department\']').change(function()
-    {
-        toggleDepartmentFields();
-    });
-
-    function toggleDepartmentFields()
+function toggleDepartmentFields()
     {
         var selectedDepartment = "";
-        var selected = $('input[type=\'radio\'][name=\'department\']:checked');
+        var selected = jQuery('input[type=\'radio\'][name=\'department\']:checked');
         
-        $('.sales-only').hide();
-        $('.lettings-only').hide();
+        jQuery('.sales-only').hide();
+        jQuery('.lettings-only').hide();
         
         if (selected.length > 0)
         {
             selectedDepartment = selected.val();
-            
+
+            // controls won't always be display:block so we should get the 
+            // first visible component (that isnt sales/lettings-only) and 
+            // use that display
+            var display = 'block';
+            jQuery('form.property-search-form .control').each(function()
+            {
+                if (!jQuery(this).hasClass('.sales-only') && !jQuery(this).hasClass('.lettings-only') && jQuery(this).css('display') != 'none')
+                {
+                    display = jQuery(this).css('display');
+                }
+            });
+
             if (selectedDepartment == 'residential-sales')
             {
-                $('.sales-only').show();
+                jQuery('.sales-only').css('display', display);
             }
             else if (selectedDepartment == 'residential-lettings')
             {
-                $('.lettings-only').show();
+                jQuery('.lettings-only').css('display', display);
             }
         }
     }
 
+jQuery( function(jQuery){
+
+    
+    // Orderby
+    jQuery( '.propertyhive-ordering' ).on( 'change', 'select.orderby', function() {
+        jQuery( this ).closest( 'form' ).submit();
+    });
+    
+    toggleDepartmentFields();
+    
+    jQuery('input[type=\'radio\'][name=\'department\']').change(function()
+    {
+        toggleDepartmentFields();
+    });
+
 });
 
+jQuery(window).resize(function() {
+    toggleDepartmentFields();
+});
