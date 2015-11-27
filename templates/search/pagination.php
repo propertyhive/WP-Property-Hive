1de7dd30c0130bb271e87bad974c73c20d7b1dce
propertyhive <info@wp-property-hive.com>
2015-11-27 13:59:37 +0000

    Always output pagination wrapper div even if paging isn't actually shown

--- templates/search/pagination.php
+++ templates/search/pagination.php
@@ -10,22 +10,26 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $wp_query;
-
-if ( $wp_query->max_num_pages <= 1 )
-	return;
 ?>
-<nav class="propertyhive-pagination">
+<div class="propertyhive-pagination">
 	<?php
-		echo paginate_links( apply_filters( 'propertyhive_pagination_args', array(
-			'base'         => str_replace( 999999999, '%#%', get_pagenum_link( 999999999 ) ),
-			'format'       => '',
-			'current'      => max( 1, get_query_var( 'paged' ) ),
-			'total'        => $wp_query->max_num_pages,
-			'prev_text'    => '&larr;',
-			'next_text'    => '&rarr;',
-			'type'         => 'list',
-			'end_size'     => 3,
-			'mid_size'     => 3
-		) ) );
+		if ( $wp_query->max_num_pages  > 1 )
+		{
+			echo paginate_links( apply_filters( 'propertyhive_pagination_args', array(
+				'base'         => str_replace( 999999999, '%#%', get_pagenum_link( 999999999 ) ),
+				'format'       => '',
+				'current'      => max( 1, get_query_var( 'paged' ) ),
+				'total'        => $wp_query->max_num_pages,
+				'prev_text'    => '&larr;',
+				'next_text'    => '&rarr;',
+				'type'         => 'list',
+				'end_size'     => 3,
+				'mid_size'     => 3
+			) ) );
+		}
+		else
+		{
+			echo '&nbsp;';
+		}
 	?>
-</nav>
+</div>
