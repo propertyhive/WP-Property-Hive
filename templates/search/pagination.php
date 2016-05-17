cfc4aef62b241a49a316fd1aa78117fd73eb4fd6
propertyhive <info@wp-property-hive.com>
2016-05-17 16:03:37 +0100

    Fix to pagination where sometimes you couldn't navigate beyond page one

--- templates/search/pagination.php
+++ templates/search/pagination.php
@@ -16,7 +16,7 @@ global $wp_query;
 		if ( $wp_query->max_num_pages  > 1 )
 		{
 			echo paginate_links( apply_filters( 'propertyhive_pagination_args', array(
-				'base'         => str_replace( 999999999, '%#%', get_pagenum_link( 999999999 ) ),
+				'base'         => esc_url_raw( str_replace( 999999999, '%#%', get_pagenum_link( 999999999, false ) ) ),
 				'format'       => '',
 				'current'      => max( 1, get_query_var( 'paged' ) ),
 				'total'        => $wp_query->max_num_pages,
