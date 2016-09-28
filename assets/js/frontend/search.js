babab850665a86d9980a42da7121182bd2480316
Property Hive <info@wp-property-hive.com>
2016-09-28 11:13:46 +0100

    Updated search.js to cater for when multiple search forms on same page

--- assets/js/frontend/search.js
+++ assets/js/frontend/search.js
@@ -2,61 +2,65 @@ function toggleDepartmentFields()
 {
     if (jQuery('form.property-search-form').length > 0)
     {
-        var selectedDepartment = "residential-sales"; // TODO: Use default from settings
-
-        var departmentEl = jQuery('form.property-search-form [name=\'department\']')
-        if (departmentEl.length > 0)
+        // There may be multiple forms on the page so treat each one individually
+        jQuery('form.property-search-form').each(function()
         {
-            switch (departmentEl.prop('tagName').toLowerCase())
-            {
-                case "select":
-                {
-                    var selected = departmentEl;
-                    break;
-                }
-                default:
-                {
-                    var selected = departmentEl.filter(':checked');
-                }
-            }
-        }
+            var selectedDepartment = "residential-sales"; // TODO: Use default from settings
 
-        jQuery('.sales-only').hide();
-        jQuery('.lettings-only').hide();
-        jQuery('.residential-only').hide();
-        jQuery('.commercial-only').hide();
-        
-        if (selected.length > 0)
-        {
-            selectedDepartment = selected.val();
-
-            // controls won't always be display:block so we should get the 
-            // first visible component (that isnt sales/lettings-only) and 
-            // use that display
-            var display = 'block';
-            jQuery('form.property-search-form .control').each(function()
+            var departmentEl = jQuery(this).find('[name=\'department\']')
+            if (departmentEl.length > 0)
             {
-                if (!jQuery(this).hasClass('.sales-only') && !jQuery(this).hasClass('.lettings-only') && !jQuery(this).hasClass('.residential-only') && !jQuery(this).hasClass('.commercial-only') && jQuery(this).css('display') != 'none')
+                switch (departmentEl.prop('tagName').toLowerCase())
                 {
-                    display = jQuery(this).css('display');
+                    case "select":
+                    {
+                        var selected = departmentEl;
+                        break;
+                    }
+                    default:
+                    {
+                        var selected = departmentEl.filter(':checked');
+                    }
                 }
-            });
+            }
 
-            if (selectedDepartment == 'residential-sales')
+            jQuery(this).find('.sales-only').hide();
+            jQuery(this).find('.lettings-only').hide();
+            jQuery(this).find('.residential-only').hide();
+            jQuery(this).find('.commercial-only').hide();
+            
+            if (selected.length > 0)
             {
-                jQuery('.sales-only').css('display', display);
-                jQuery('.residential-only').css('display', display);
+                selectedDepartment = selected.val();
+
+                // controls won't always be display:block so we should get the 
+                // first visible component (that isnt sales/lettings-only) and 
+                // use that display
+                var display = 'block';
+                jQuery(this).find('.control').each(function()
+                {
+                    if (!jQuery(this).hasClass('.sales-only') && !jQuery(this).hasClass('.lettings-only') && !jQuery(this).hasClass('.residential-only') && !jQuery(this).hasClass('.commercial-only') && jQuery(this).css('display') != 'none')
+                    {
+                        display = jQuery(this).css('display');
+                    }
+                });
+
+                if (selectedDepartment == 'residential-sales')
+                {
+                    jQuery(this).find('.sales-only').css('display', display);
+                    jQuery(this).find('.residential-only').css('display', display);
+                }
+                else if (selectedDepartment == 'residential-lettings')
+                {
+                    jQuery(this).find('.lettings-only').css('display', display);
+                    jQuery(this).find('.residential-only').css('display', display);
+                }
+                else if (selectedDepartment == 'commercial')
+                {
+                    jQuery(this).find('.commercial-only').css('display', display);
+                }
             }
-            else if (selectedDepartment == 'residential-lettings')
-            {
-                jQuery('.lettings-only').css('display', display);
-                jQuery('.residential-only').css('display', display);
-            }
-            else if (selectedDepartment == 'commercial')
-            {
-                jQuery('.commercial-only').css('display', display);
-            }
-        }
+        });
     }
 }
 
