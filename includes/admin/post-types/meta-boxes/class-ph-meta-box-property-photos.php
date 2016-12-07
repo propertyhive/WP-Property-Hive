54c5513ac4ca3e645e770385b225cc313e6c62a2
Property Hive <info@wp-property-hive.com>
2016-12-07 10:39:36 +0000

    Added ability to edit existing photos

--- includes/admin/post-types/meta-boxes/class-ph-meta-box-property-photos.php
+++ includes/admin/post-types/meta-boxes/class-ph-meta-box-property-photos.php
@@ -87,7 +87,6 @@ class PH_Meta_Box_Property_Photos {
                       photo_id = photo_id.replace(\'photo_\', \'\');
                       
                       var attachment_ids = jQuery(\'#photo_attachment_ids\').val();
-                      // Check it\'s not already in the list
                       attachment_ids = attachment_ids.split(\',\');
                       
                       var new_attachment_ids = \'\';
@@ -114,7 +113,75 @@ class PH_Meta_Box_Property_Photos {
                   
                   jQuery(\'body\').on(\'click\', \'#property_photos_grid .attachment-edit a\', function()
                   {
+                      event.preventDefault();
+
+                      var container = jQuery(this).parent().parent().parent();
+                      var photo_id = container.attr(\'id\');
+                      photo_id = photo_id.replace(\'photo_\', \'\');
                       
+                      // Create the media frame.
+                    file_frame = wp.media.frames.file_frame = wp.media({
+                      title: jQuery( this ).data( \'uploader_title\' ),
+                      button: {
+                        text: \'Update Image\',
+                      },
+                      multiple: false
+                    });
+
+                    // open
+                    file_frame.on(\'open\',function() {
+                      
+                      var selection = file_frame.state().get(\'selection\');
+                      var ids = photo_id;
+                      if (ids) {
+                          idsArray = ids.split(',');
+                          idsArray.forEach(function(id) {
+                              attachment = wp.media.attachment(id);
+                              attachment.fetch();
+                              selection.add( attachment ? [ attachment ] : [] );
+                          });
+                      }
+                          
+                    });
+
+                    // When an image is selected, run a callback.
+                    file_frame.on( \'select\', function() {
+                        var selection = file_frame.state().get(\'selection\');
+     
+                        selection.map( function( attachment ) {
+
+                            attachment = attachment.toJSON();
+
+                            if (attachment.id != photo_id)
+                            {
+                              // Replace photo
+                              var attachment_ids = jQuery(\'#photo_attachment_ids\').val();
+                              attachment_ids = attachment_ids.split(\',\');
+                              
+                              for (var i in attachment_ids)
+                              {
+                                  if (attachment_ids[i] == photo_id)
+                                  {
+                                      attachment_ids[i] = attachment.id;
+                                  }
+                              }
+                              jQuery(\'#photo_attachment_ids\').val(attachment_ids);
+                              
+                              // Add photo to media grid
+                              var mediaHTML = \'\';
+                              mediaHTML += \'<li id="photo_\' + attachment.id + \'">\';
+                              mediaHTML += \'<div class="hover"><div class="attachment-delete"><a href=""></a></div><div class="attachment-edit"><a href=""></a></div></div>\';
+                              mediaHTML += \'<img src="\' + attachment.url + \'" alt=""></li>\';
+                              
+                              jQuery(\'#property_photos_grid ul li#photo_\' + photo_id).after(mediaHTML);
+                              jQuery(\'#property_photos_grid ul li#photo_\' + photo_id).remove();
+
+                            }
+                        });
+                    });
+
+                    // Finally, open the modal
+                    file_frame.open();
                   });
                   
                   jQuery(\'.ph_upload_photo_button\').live(\'click\', function( event ){
@@ -144,9 +211,6 @@ class PH_Meta_Box_Property_Photos {
                      
                             attachment = attachment.toJSON();
                      
-                            // Do something with attachment.id and/or attachment.url here
-                            console.log(attachment.url);
-                            
                             // Add selected images to grid
                             add_photo_attachment_to_grid(attachment);
                         });
