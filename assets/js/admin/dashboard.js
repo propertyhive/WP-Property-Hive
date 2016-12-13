331e3855d75903a48560827659f356bee91e355e
Property Hive <info@wp-property-hive.com>
2016-12-13 21:19:09 +0000

    Small adjustment to layout of dashboard PH news widget

--- assets/js/admin/dashboard.js
+++ assets/js/admin/dashboard.js
@@ -17,7 +17,7 @@ jQuery(window).load(function()
 		jQuery('#ph_dashboard_news').html('<ul></ul>')
 		for ( var i in response )
 		{
-			jQuery('#ph_dashboard_news ul').append('<li><a href="' + response[i].permalink + '">' + response[i].title + '</a> ' + response[i].date + '</li>');
+			jQuery('#ph_dashboard_news ul').append('<li><a class="rsswidget" style="font-weight:400" href="' + response[i].permalink + '">' + response[i].title + '</a><br><small style="opacity:0.85">' + response[i].date + '</small></li>');
 		}
 		
 	}, 'json');
