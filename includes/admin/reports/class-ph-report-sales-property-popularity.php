fc9ddf4869514d6b6849e60c93824f0db5f1b712
Property Hive <info@wp-property-hive.com>
2017-01-28 13:58:16 +0000

    Allow for third party stats via filter and fixes to % stats shown

--- includes/admin/reports/class-ph-report-sales-property-popularity.php
+++ includes/admin/reports/class-ph-report-sales-property-popularity.php
@@ -49,49 +49,57 @@ class PH_Report_Sales_Property_Popularity extends PH_Admin_Report {
 			{
 				$property_query->the_post();
 
-				$view_statistics = get_post_meta( get_the_ID(), '_view_statistics', TRUE );
+				$sources = array(
+					'Website' => '_view_statistics'
+				);
 
-				$total_views = 0;
-				$previous_time_frame = false;
+				$sources = apply_filters( 'propertyhive_property_view_statistic_sources', $sources );
 
-				if ( is_array($view_statistics) && !empty($view_statistics) )
+				foreach ( $sources as $label => $meta_key )
 				{
-					$date_from = $original_date_from;
-					$date_to = $original_date_to;
+					$view_statistics = get_post_meta( get_the_ID(), $meta_key, TRUE );
 
-					while (strtotime($date_from) <= strtotime($date_to)) 
+					$total_views = 0;
+					$previous_time_frame = false;
+
+					if ( is_array($view_statistics) && !empty($view_statistics) )
 					{
-						if ( isset( $view_statistics[$date_from] ) )
+						$date_from = $original_date_from;
+						$date_to = $original_date_to;
+
+						while (strtotime($date_from) <= strtotime($date_to)) 
 						{
-							$total_views += $view_statistics[$date_from];
+							if ( isset( $view_statistics[$date_from] ) )
+							{
+								$total_views += $view_statistics[$date_from];
+							}
+			                $date_from = date("Y-m-d", strtotime("+1 day", strtotime($date_from)));
 						}
-		                $date_from = date("Y-m-d", strtotime("+1 day", strtotime($date_from)));
+
+						$date_from = $original_date_from;
+						$date_to = $original_date_to;
+
+						$date_from = date("Y-m-d", strtotime($original_date_from) - $secs_diff - 86400);
+						$date_to = date("Y-m-d", strtotime($date_to) - $secs_diff - 86400);
+
+						while (strtotime($date_from) <= strtotime($date_to)) 
+						{
+							if ( isset( $view_statistics[$date_from] ) )
+							{
+								if ( $previous_time_frame === false ) { $previous_time_frame = 0; }
+								$previous_time_frame += $view_statistics[$date_from];
+							}
+			                $date_from = date("Y-m-d", strtotime("+1 day", strtotime($date_from)));
+						}
 					}
 
-					$date_from = $original_date_from;
-					$date_to = $original_date_to;
+					if ( !isset($return[$label]) ) { $return[$label] = array(); }
 
-					$date_from = date("Y-m-d", strtotime($original_date_from) - $secs_diff - 86400);
-					$date_to = date("Y-m-d", strtotime($date_to) - $secs_diff - 86400);
-
-					while (strtotime($date_from) <= strtotime($date_to)) 
-					{
-						if ( isset( $view_statistics[$date_from] ) )
-						{
-							if ( $previous_time_frame === false ) { $previous_time_frame = 0; }
-							$previous_time_frame += $view_statistics[$date_from];
-						}
-		                $date_from = date("Y-m-d", strtotime("+1 day", strtotime($date_from)));
-					}
-				}
-
-				//if ( $total_views > 0 || $previous_time_frame !== FALSE )
-				//{
-					$return[get_the_ID()] = array(
+					$return[$label][get_the_ID()] = array(
 						'total_views' => $total_views,
 						'previous_time_frame' => $previous_time_frame
 					);
-				//}
+				}
 			}
 		}
 		return $return;
@@ -104,19 +112,22 @@ class PH_Report_Sales_Property_Popularity extends PH_Admin_Report {
 
 		$duration = ( ( isset($_GET['duration']) ) ? ($_GET['duration'] - 1) : 6 );
 
-		$date_from = ( ( isset($_GET['date_from']) ) ? $_GET['date_from'] : date("Y-m-d", strtotime($duration . " days ago")) );
-		$date_to = ( ( isset($_GET['date_to']) ) ? $_GET['date_to'] : date("Y-m-d") );
+		$date_from = ( ( isset($_GET['date_from']) ) ? $_GET['date_from'] : date("Y-m-d", strtotime(($duration + 1) . " days ago")) );
+		$date_to = ( ( isset($_GET['date_to']) ) ? $_GET['date_to'] : date("Y-m-d", strtotime("yesterday")) );
 
-		$property_view_data = $this->get_property_view_data( $date_from, $date_to );
+		$properties_view_data = $this->get_property_view_data( $date_from, $date_to );
 
 		// Order by total views
 		$popular_order = array();
-		foreach ( $property_view_data as $property_id => $view_totals )
+		foreach ( $properties_view_data as $label => $property_view_data )
 		{
-			$popular_order[$view_totals['total_views'].'.'.$property_id] = $property_id;
-		}
+			foreach ( $property_view_data as $property_id => $view_totals )
+			{
+				if ( !isset($popular_order[$label]) ) { $popular_order[$label] = array(); }
 
-		krsort($popular_order);
+				$popular_order[$label][$view_totals['total_views'].'.'.$property_id] = $property_id;
+			}
+		}
 ?>
 
 <style type="text/css">
@@ -128,8 +139,9 @@ class PH_Report_Sales_Property_Popularity extends PH_Admin_Report {
 .chart-tabs ul li.active a { background:#FFF; font-weight:700; }
 .chart-tabs ul li a:hover { background:#FFF; }
 .chart-container { padding:12px 12px 12px 12px; background:#FFF; border:1px solid #DDD; }
-.chart-panel { float:left; width:48%; border:1px solid #DDD; padding:0 12px 12px 12px; box-sizing:border-box; }
-.chart-container .chart-panel:nth-child(2) { float:right;  }
+.chart-panel { float:left; width:49%; border:1px solid #DDD; padding:0 12px 12px 12px; box-sizing:border-box; margin-bottom:20px; }
+.chart-container .chart-panels {  }
+.chart-container .chart-panel:nth-child(even) { float:right; }
 .chart-container .chart-panel ul li { padding:8px; margin:0; }
 .chart-container .chart-panel ul li:nth-child(odd) { background:#EEE; }
 .chart-container .chart-panel ul li:nth-child(1) { background:#FFF; font-weight:700; border-bottom:1px solid #DDD; }
@@ -142,7 +154,7 @@ class PH_Report_Sales_Property_Popularity extends PH_Admin_Report {
 
 <div class="chart-tabs">
 	<ul>
-		<li<?php if ( $duration == 0 ) { echo ' class="active"'; } ?>><a href="admin.php?page=ph-reports&tab=properties&report=sales_property_popularity&duration=1">Today</a></li>
+		<li<?php if ( $duration == 0 ) { echo ' class="active"'; } ?>><a href="admin.php?page=ph-reports&tab=properties&report=sales_property_popularity&duration=1">Yesterday</a></li>
 		<li<?php if ( $duration == 6 ) { echo ' class="active"'; } ?>><a href="admin.php?page=ph-reports&tab=properties&report=sales_property_popularity&duration=7">Last 7 Days</a></li>
 		<li<?php if ( $duration == 29 ) { echo ' class="active"'; } ?>><a href="admin.php?page=ph-reports&tab=properties&report=sales_property_popularity&duration=30">Last 30 Days</a></li>
 	</ul>
@@ -150,24 +162,103 @@ class PH_Report_Sales_Property_Popularity extends PH_Admin_Report {
 
 <div class="chart-container">
 
-	<div class="chart-panel">
-		
-		<h3>Most Popular Properties</h3>
+	<?php
+		foreach ( $popular_order as $label => $order )
+		{
+			$property_view_data = $properties_view_data[$label];
 
-		<?php
-			if ( !empty($popular_order) )
-			{
-				$slice_offset = min(10, ceil(count($popular_order) / 2));
-				$most_popular = array_slice($popular_order, 0, $slice_offset);
+			krsort($order);
+	?>
 
-				echo '<ul>
-					<li>
-						Property
-						<div class="stat">Views</div>
-					</li>';
-				foreach ( $most_popular as $key => $property_id )
+	<div class="chart-panels">
+
+		<div class="chart-panel">
+			
+			<h3>Most Popular Properties - <?php echo $label; ?></h3>
+
+			<?php
+				if ( !empty($order) )
 				{
-					if ( $property_view_data[$property_id]['total_views'] > 0 )
+					$slice_offset = min(10, ceil(count($order) / 2));
+					$most_popular = array_slice($order, 0, $slice_offset);
+
+					echo '<ul>
+						<li>
+							Property
+							<div class="stat">Views</div>
+						</li>';
+					foreach ( $most_popular as $key => $property_id )
+					{
+						if ( $property_view_data[$property_id]['total_views'] > 0 )
+						{
+							$property = new PH_Property($property_id);
+
+							echo '<li>
+								<a href="' . get_edit_post_link($property_id) . '">' . $property->get_formatted_full_address() . '</a>
+								<div class="stat">' . $property_view_data[$property_id]['total_views'] . ' <span>';
+							if ( $property_view_data[$property_id]['total_views'] == $property_view_data[$property_id]['previous_time_frame'] )
+							{
+								echo '<span style="color:#999" title="No views"><span class="dashicons dashicons-arrow-right" style="color:#999"></span>-</span>';
+							}
+							elseif ( $property_view_data[$property_id]['previous_time_frame'] === false || $property_view_data[$property_id]['previous_time_frame'] == 0 )
+							{
+								echo '<span style="color:#090" title="No views in previous timeframe"><span class="dashicons dashicons-arrow-up" style="color:#090"></span>&#x221e;</span>';
+							}
+							else
+							{
+								if ( $property_view_data[$property_id]['total_views'] >= $property_view_data[$property_id]['previous_time_frame'] )
+								{
+									$percentage_difference = $property_view_data[$property_id]['total_views'] / $property_view_data[$property_id]['previous_time_frame'];
+									$percentage_difference = 100 - ($percentage_difference * 100);
+									if ( $percentage_difference < 0 ) { $percentage_difference = $percentage_difference * -1; }
+									$percentage_difference = number_format($percentage_difference, 1);
+
+									echo '<span style="color:#090" title="' . $property_view_data[$property_id]['previous_time_frame'] . ' views in previous timeframe"><span class="dashicons dashicons-arrow-up" style="color:#090"></span>' . $percentage_difference . '%</span>';
+								}
+								else
+								{
+									$percentage_difference = $property_view_data[$property_id]['total_views'] / $property_view_data[$property_id]['previous_time_frame'];
+									$percentage_difference = 100 - ($percentage_difference * 100);
+									if ( $percentage_difference < 0 ) { $percentage_difference = $percentage_difference * -1; }
+									$percentage_difference = number_format($percentage_difference, 1);
+
+									echo '<span style="color:#900" title="' . $property_view_data[$property_id]['previous_time_frame'] . ' views in previous timeframe"><span class="dashicons dashicons-arrow-down" style="color:#900"></span>' . $percentage_difference . '%</span>';
+								}
+							}
+							echo '</span></div>
+							</li>';
+
+							unset($order[$key]);
+						}
+					}
+					echo '</ul>';
+				}
+				else
+				{
+					echo '<p>No view statistics available for ' . $label . '</p>';
+				}
+			?>
+
+		</div>
+
+		<div class="chart-panel">
+			
+			<h3>Least Popular Properties - <?php echo $label; ?></h3>
+
+			<?php
+				if ( !empty($order) )
+				{
+					ksort($order, SORT_NUMERIC);
+
+					$slice_offset = min(10, ceil(count($order) / 2));
+					$least_popular = array_slice($order, 0, $slice_offset);
+
+					echo '<ul>
+						<li>
+							Property
+							<div class="stat">Views</div>
+						</li>';
+					foreach ( $least_popular as $key => $property_id )
 					{
 						$property = new PH_Property($property_id);
 
@@ -187,7 +278,8 @@ class PH_Report_Sales_Property_Popularity extends PH_Admin_Report {
 							if ( $property_view_data[$property_id]['total_views'] >= $property_view_data[$property_id]['previous_time_frame'] )
 							{
 								$percentage_difference = $property_view_data[$property_id]['total_views'] / $property_view_data[$property_id]['previous_time_frame'];
-								$percentage_difference = $percentage_difference * 100;
+								$percentage_difference = 100 - ($percentage_difference * 100);
+								if ( $percentage_difference < 0 ) { $percentage_difference = $percentage_difference * -1; }
 								$percentage_difference = number_format($percentage_difference, 1);
 
 								echo '<span style="color:#090" title="' . $property_view_data[$property_id]['previous_time_frame'] . ' views in previous timeframe"><span class="dashicons dashicons-arrow-up" style="color:#090"></span>' . $percentage_difference . '%</span>';
@@ -195,7 +287,8 @@ class PH_Report_Sales_Property_Popularity extends PH_Admin_Report {
 							else
 							{
 								$percentage_difference = $property_view_data[$property_id]['total_views'] / $property_view_data[$property_id]['previous_time_frame'];
-								$percentage_difference = $percentage_difference * 100;
+								$percentage_difference = 100 - ($percentage_difference * 100);
+								if ( $percentage_difference < 0 ) { $percentage_difference = $percentage_difference * -1; }
 								$percentage_difference = number_format($percentage_difference, 1);
 
 								echo '<span style="color:#900" title="' . $property_view_data[$property_id]['previous_time_frame'] . ' views in previous timeframe"><span class="dashicons dashicons-arrow-down" style="color:#900"></span>' . $percentage_difference . '%</span>';
@@ -204,86 +297,25 @@ class PH_Report_Sales_Property_Popularity extends PH_Admin_Report {
 						echo '</span></div>
 						</li>';
 
-						unset($popular_order[$key]);
+						unset($order[$key]);
 					}
+					echo '</ul>';
 				}
-				echo '</ul>';
-			}
-			else
-			{
-				echo '<p>No view statistics available</p>';
-			}
-		?>
-
-	</div>
-
-	<div class="chart-panel">
-		
-		<h3>Least Popular Properties</h3>
-
-		<?php
-			if ( !empty($popular_order) )
-			{
-				ksort($popular_order, SORT_NUMERIC);
-
-				$slice_offset = min(10, ceil(count($popular_order) / 2));
-				$least_popular = array_slice($popular_order, 0, $slice_offset);
-
-				echo '<ul>
-					<li>
-						Property
-						<div class="stat">Views</div>
-					</li>';
-				foreach ( $least_popular as $key => $property_id )
+				else
 				{
-					$property = new PH_Property($property_id);
-
-					echo '<li>
-						<a href="' . get_edit_post_link($property_id) . '">' . $property->get_formatted_full_address() . '</a>
-						<div class="stat">' . $property_view_data[$property_id]['total_views'] . ' <span>';
-					if ( $property_view_data[$property_id]['total_views'] == $property_view_data[$property_id]['previous_time_frame'] )
-					{
-						echo '<span style="color:#999" title="No views"><span class="dashicons dashicons-arrow-right" style="color:#999"></span>-</span>';
-					}
-					elseif ( $property_view_data[$property_id]['previous_time_frame'] === false || $property_view_data[$property_id]['previous_time_frame'] == 0 )
-					{
-						echo '<span style="color:#090" title="No views in previous timeframe"><span class="dashicons dashicons-arrow-up" style="color:#090"></span>&#x221e;</span>';
-					}
-					else
-					{
-						if ( $property_view_data[$property_id]['total_views'] >= $property_view_data[$property_id]['previous_time_frame'] )
-						{
-							$percentage_difference = $property_view_data[$property_id]['total_views'] / $property_view_data[$property_id]['previous_time_frame'];
-							$percentage_difference = $percentage_difference * 100;
-							$percentage_difference = number_format($percentage_difference, 1);
-
-							echo '<span style="color:#090" title="' . $property_view_data[$property_id]['previous_time_frame'] . ' views in previous timeframe"><span class="dashicons dashicons-arrow-up" style="color:#090"></span>' . $percentage_difference . '%</span>';
-						}
-						else
-						{
-							$percentage_difference = $property_view_data[$property_id]['total_views'] / $property_view_data[$property_id]['previous_time_frame'];
-							$percentage_difference = $percentage_difference * 100;
-							$percentage_difference = number_format($percentage_difference, 1);
-
-							echo '<span style="color:#900" title="' . $property_view_data[$property_id]['previous_time_frame'] . ' views in previous timeframe"><span class="dashicons dashicons-arrow-down" style="color:#900"></span>' . $percentage_difference . '%</span>';
-						}
-					}
-					echo '</span></div>
-					</li>';
-
-					unset($popular_order[$key]);
+					echo '<p>No view statistics available ' . $label . '</p>';
 				}
-				echo '</ul>';
-			}
-			else
-			{
-				echo '<p>No view statistics available</p>';
-			}
-		?>
+			?>
+
+		</div>
+
+		<div style="clear:both"></div>
 
 	</div>
 
-	<div style="clear:both"></div>
+	<?php
+		}
+	?>
 
 </div>
 
