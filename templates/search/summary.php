ba3eedceaef2826bb537b1fa42f7459289cffc6a
Property Hive <info@wp-property-hive.com>
2018-11-13 08:49:39 +0000

    New filter 'propertyhive_search_summary_length' to change summary length

--- templates/search/summary.php
+++ templates/search/summary.php
@@ -10,12 +10,14 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $property;
+
+$summary_length = apply_filters('propertyhive_search_summary_length', 300);
 ?>
 <div class="summary">
 
 	<?php 
-		echo substr(strip_tags($property->post_excerpt), 0, 300);
-		if ( strlen(strip_tags($property->post_excerpt)) > 300 ) { echo '...'; } 
+		echo substr(strip_tags($property->post_excerpt), 0, $summary_length);
+		if ( strlen(strip_tags($property->post_excerpt)) > $summary_length ) { echo '...'; } 
 	?>
 
 </div>
