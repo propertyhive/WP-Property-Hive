c21bacd49d78b7dcd4f915034fdedad71592159e
Zaid Crowe <crowe.zaid@gmail.com>
2019-02-07 10:16:02 +0000

    Removed junk functions

--- assets/js/admin/meta-boxes.js
+++ assets/js/admin/meta-boxes.js
@@ -160,32 +160,3 @@ function initialise_datepicker() {
         }
     });
 }
-
-function restrict_phone_number() {
-    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
-        if (inputFilter(this.value)) {
-            this.oldValue = this.value;
-            this.oldSelectionStart = this.selectionStart;
-            this.oldSelectionEnd = this.selectionEnd;
-        } else if (this.hasOwnProperty("oldValue")) {
-            this.value = this.oldValue;
-            this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-        }
-    });
-}
-
-// Restricts input for each element in the set of matched elements to the given inputFilter.
-(function($) {
-  $.fn.inputFilter = function(inputFilter) {
-    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
-      if (inputFilter(this.value)) {
-        this.oldValue = this.value;
-        this.oldSelectionStart = this.selectionStart;
-        this.oldSelectionEnd = this.selectionEnd;
-      } else if (this.hasOwnProperty("oldValue")) {
-        this.value = this.oldValue;
-        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-      }
-    });
-  };
-}(jQuery));
