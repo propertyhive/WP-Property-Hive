1725723efb6b0aef84fa18e8b23b63896464a1fb
Property Hive <info@wp-property-hive.com>
2019-08-15 08:57:34 +0100

    Add actions to appraisal, offer, viewing & sales to handle custom fields

--- includes/admin/post-types/meta-boxes/class-ph-meta-box-offer-details.php
+++ includes/admin/post-types/meta-boxes/class-ph-meta-box-offer-details.php
@@ -68,6 +68,8 @@ function redraw_offer_details_meta_box()
 
         $amount = preg_replace("/[^0-9]/", '', ph_clean($_POST['_amount']));
         update_post_meta( $post_id, '_amount', $amount );
+
+        do_action( 'propertyhive_save_offer_details', $post_id );
     }
 
 }
