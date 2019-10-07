d0d76bbf278ad46c59d9b35ad76819c50d55f1af
Property Hive <info@wp-property-hive.com>
2019-10-07 04:50:38 +0100

    Add new dashboard widget showing users next 10 upcoming appointments

--- assets/js/admin/dashboard.js
+++ assets/js/admin/dashboard.js
@@ -46,4 +46,29 @@ jQuery(window).load(function()
 			
 		}, 'json');
 	}
+
+	// Load my upcoming appointments, if div is present (i.e. if viewings module etc hasn't been disabled)
+	if ( jQuery('#ph_dashboard_my_upcoming_appointments').length > 0 )
+	{
+		var data = {
+			action: 'propertyhive_get_my_upcoming_appointments',
+			security: 'get-my-upcoming-appointments'
+		};
+
+		jQuery.post(propertyhive_dashboard.ajax_url, data, function(response)
+		{
+			if ( response == '' || response.length == 0 )
+			{
+				jQuery('#ph_dashboard_my_upcoming_appointments').html('No upcoming appointments found');
+				return;
+			}
+
+			jQuery('#ph_dashboard_my_upcoming_appointments').html('<ul></ul>')
+			for ( var i in response )
+			{
+				jQuery('#ph_dashboard_my_upcoming_appointments ul').append('<li><a class="rsswidget" style="font-weight:400" href="' + response[i].edit_link + '">' + response[i].title + '</a><br><small style="opacity:0.85">' + response[i].start_date_time_formatted_Hi_jSFY + '</small></li>');
+			}
+			
+		}, 'json');
+	}
 });
