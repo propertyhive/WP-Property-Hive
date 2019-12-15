0d92a67f8d9f4b99dfe4608031c9df6ec8aace2f
Property Hive <info@wp-property-hive.com>
2019-12-15 18:10:44 +0000

    First draft of tenancy safety checks tab/meta box complete

--- includes/admin/post-types/class-ph-admin-meta-boxes.php
+++ includes/admin/post-types/class-ph-admin-meta-boxes.php
@@ -104,6 +104,7 @@ class PH_Admin_Meta_Boxes {
         add_action( 'propertyhive_process_tenancy_meta', 'PH_Meta_Box_Tenancy_Applicant::save', 15, 2 );
         add_action( 'propertyhive_process_tenancy_meta', 'PH_Meta_Box_Tenancy_Property::save', 20, 2 );
         add_action( 'propertyhive_process_tenancy_meta', 'PH_Meta_Box_Tenancy_Deposit::save', 25, 2 );
+        add_action( 'propertyhive_process_tenancy_meta', 'PH_Meta_Box_Tenancy_Safety_Checks::save', 30, 2 );
 
 		// Error handling (for showing errors from meta boxes on next page load)
 		add_action( 'admin_notices', array( $this, 'output_errors' ) );
--- includes/admin/post-types/meta-boxes/class-ph-meta-box-tenancy-safety-checks.php
+++ includes/admin/post-types/meta-boxes/class-ph-meta-box-tenancy-safety-checks.php
@@ -18,12 +18,58 @@ class PH_Meta_Box_Tenancy_Safety_Checks {
 	 */
 	public static function output( $post ) {
         global $wpdb, $thepostid;
+
+        $safety_checks = get_post_meta( $thepostid, '_safety_checks', TRUE );
         
         echo '<div class="propertyhive_meta_box">';
         
         echo '<div class="options_group">';
         
-       
+        $safety_check_types = array(
+            'Gas',
+            'Electricity',
+            'Smoke Alarm',
+            'Carbon Monoxide',
+            'Fire',
+            'Buildings Insurance',
+            'Contents Insurance',
+            'EPC',
+            'HMO',
+            'PAT',
+        );
+
+        $safety_check_types = apply_filters( 'propertyhive_tenancy_safety_check_types', $safety_check_types );
+
+        foreach ( $safety_check_types as $i => $safety_check_type )
+        {
+            $description = '';
+            if ( 
+                isset($safety_checks[$safety_check_type]) &&
+                $safety_checks[$safety_check_type] != ''
+            )
+            {
+                if ( strtotime($safety_checks[$safety_check_type]) <= time() )
+                {
+                    $description = ' <span style="color:#C00; font-weight:700">Expired</span>';
+                }
+                elseif ( strtotime($safety_checks[$safety_check_type]) <= time() + (30 * 24 * 60 * 60) )
+                {
+                    $description = ' <span style="color:#ffbf00; font-weight:700">Expiring Soon</span>';
+                }
+            }
+
+            $args = array( 
+                'id' => '_safety_check_' . $i, 
+                'name' => '_safety_check[' . $safety_check_type . ']', 
+                'label' => __( $safety_check_type . ' Expiry', 'propertyhive' ), 
+                'value' => isset($safety_checks[$safety_check_type]) ? $safety_checks[$safety_check_type] : '',
+                'desc_tip' => false, 
+                'description' => $description,
+                'class' => 'small',
+                'type' => 'date',
+            );
+            propertyhive_wp_text_input( $args );
+        }
 
         echo '</div>';
         
@@ -37,7 +83,27 @@ class PH_Meta_Box_Tenancy_Safety_Checks {
     public static function save( $post_id, $post ) {
         global $wpdb;
 
+        $lowest_date = '';
+        $safety_checks = array();
 
+        if ( isset($_POST['_safety_check']) && is_array($_POST['_safety_check']) )
+        {
+            foreach ( $_POST['_safety_check'] as $safety_check_type => $date )
+            {
+                if ( ph_clean($date) != '' )
+                {
+                    $safety_checks[$safety_check_type] = ph_clean($date);
+
+                    if ( $lowest_date == '' || ( $lowest_date != '' && strtotime($lowest_date) > strtotime(ph_clean($date)) ) )
+                    {
+                        $lowest_date = ph_clean($date);
+                    }
+                }
+            }
+        }
+
+        update_post_meta( $post_id, '_nearest_safety_check_expiry', $lowest_date );
+        update_post_meta( $post_id, '_safety_checks', $safety_checks );
     }
 
 }
