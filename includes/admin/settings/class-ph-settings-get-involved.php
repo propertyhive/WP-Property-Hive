c749c4375d6006f08876924031f1c351bbf162d8
Eoin Garvey <67641630+egar01n@users.noreply.github.com>
2020-07-16 10:52:14 +0100

    Make use of spaces consistent

--- includes/admin/settings/class-ph-settings-get-involved.php
+++ includes/admin/settings/class-ph-settings-get-involved.php
@@ -19,40 +19,42 @@ if ( ! class_exists( 'PH_Settings_Get_Involved' ) ) :
  */
 class PH_Settings_Get_Involved extends PH_Settings_Page {
 
-	/**
-	 * Constructor.
-	 */
-	public function __construct() {
-		$this->id    = 'getinvolved';
-		$this->label = __( 'Get Involved', 'propertyhive' );
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->id    = 'getinvolved';
+        $this->label = __( 'Get Involved', 'propertyhive' );
 
-		add_filter( 'propertyhive_settings_tabs_array', array( $this, 'add_settings_page' ), 20 );
-		add_action( 'propertyhive_settings_' . $this->id, array( $this, 'output' ) );
+        add_filter( 'propertyhive_settings_tabs_array', array( $this, 'add_settings_page' ), 20 );
+        add_action( 'propertyhive_settings_' . $this->id, array( $this, 'output' ) );
     }
 
     /**
-	 * Get settings array
-	 *
-	 * @return array
-	 */
-	public function get_settings() {
+     * Get settings array
+     *
+     * @return array
+     */
+    public function get_settings()
+    {
 
         global $hide_save_button;
 
         $hide_save_button = TRUE;
 
-		return apply_filters( 'propertyhive_get_involved_settings', array(
+        return apply_filters( 'propertyhive_get_involved_settings', array(
 
-			array( 'title' => __( 'Get Involved', 'propertyhive' ), 'type' => 'title', 'desc' => '', 'id' => 'get_involved_options' ),
+            array( 'title' => __( 'Get Involved', 'propertyhive' ), 'type' => 'title', 'desc' => '', 'id' => 'get_involved_options' ),
 
             array(
                 'type'      => 'getinvolved',
             ),
 
-			array( 'type' => 'sectionend', 'id' => 'get_involved_options')
+            array( 'type' => 'sectionend', 'id' => 'get_involved_options')
 
-		) ); // End add on settings
-	}
+        ) );
+    }
 
     /**
      * Output the settings
