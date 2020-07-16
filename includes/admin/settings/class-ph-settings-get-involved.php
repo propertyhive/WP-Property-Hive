1bed178e91cb50a1e4f87601a14c60befa5ee8cd
Eoin Garvey <67641630+egar01n@users.noreply.github.com>
2020-07-16 10:42:30 +0100

    Add get_settings and output functions

--- includes/admin/settings/class-ph-settings-get-involved.php
+++ includes/admin/settings/class-ph-settings-get-involved.php
@@ -28,7 +28,41 @@ class PH_Settings_Get_Involved extends PH_Settings_Page {
 
 		add_filter( 'propertyhive_settings_tabs_array', array( $this, 'add_settings_page' ), 20 );
 		add_action( 'propertyhive_settings_' . $this->id, array( $this, 'output' ) );
+    }
+
+    /**
+	 * Get settings array
+	 *
+	 * @return array
+	 */
+	public function get_settings() {
+
+        global $hide_save_button;
+
+        $hide_save_button = TRUE;
+
+		return apply_filters( 'propertyhive_get_involved_settings', array(
+
+			array( 'title' => __( 'Get Involved', 'propertyhive' ), 'type' => 'title', 'desc' => '', 'id' => 'get_involved_options' ),
+
+            array(
+                'type'      => 'getinvolved',
+            ),
+
+			array( 'type' => 'sectionend', 'id' => 'get_involved_options')
+
+		) ); // End add on settings
 	}
+
+    /**
+     * Output the settings
+     */
+    public function output()
+    {
+        $settings = $this->get_settings();
+
+        PH_Admin_Settings::output_fields( $settings );
+    }
 }
 
 endif;
