2940395285df4ec602855b5f6181b3846d61c414
Property Hive <info@wp-property-hive.com>
2020-08-13 00:40:52 +0100

    Add filters to contact notes grid to allow third parties to hook in

--- includes/admin/post-types/meta-boxes/class-ph-meta-box-contact-notes.php
+++ includes/admin/post-types/meta-boxes/class-ph-meta-box-contact-notes.php
@@ -35,13 +35,31 @@ class PH_Meta_Box_Contact_Notes {
         $notes = get_comments( $args );
 
         echo '<ul class="subsubsub notes-filter" style="float:none; padding-left:10px;">';
-            echo '<li><a href="" data-filter-class="*" class="current">' . __( 'All', 'propertyhive' ) . '</a> |&nbsp; </li>';
-            echo '<li><a href="" data-filter-class="note-type-mailout">' . __( 'Mailout', 'propertyhive' ) . '</a> |&nbsp; </li>';
-            echo '<li><a href="" data-filter-class="note-type-note">' . __( 'Note', 'propertyhive' ) . '</a> |&nbsp; </li>';
-            echo '<li><a href="" data-filter-class="note-type-action">' . __( 'System Change', 'propertyhive' ) . '</a></li>';
+
+            $notes_filters = array(
+                '' =>  __( 'All', 'propertyhive' ),
+                'mailout' =>  __( 'Mailout', 'propertyhive' ),
+                'note' =>  __( 'Note', 'propertyhive' ),
+                'action' =>  __( 'System Change', 'propertyhive' ),
+            );
+
+            $notes_filters = apply_filters( 'propertyhive_notes_filters', $notes_filters, $post );
+            $notes_filters = apply_filters( 'propertyhive_contact_notes_filters', $notes_filters, $post );
+
+            $i = 0;
+            foreach ( $notes_filters as $class => $label )
+            {
+                echo '<li><a href="" data-filter-class="' . ( $class == '' ? '*' : 'note-type-' . $class ) . '">' . $label . '</a>';
+                if ( $i < count($notes_filters) - 1 ) { echo ' |&nbsp; '; }
+                echo '</li>';
+                ++$i;
+            }
+            
         echo '</ul>';
 
         echo '<ul class="record_notes" style="max-height:300px; overflow-y:auto">';
+
+        $note_output = array();
 
         if ( !empty($notes) ) 
         {
@@ -49,11 +67,7 @@ class PH_Meta_Box_Contact_Notes {
 
             foreach( $notes as $note ) 
             {
-                $note_classes = array( 'note' );
-
                 $comment_content = unserialize($note->comment_content);
-
-                $note_classes[] = 'note-type-' . $comment_content['note_type'];
 
                 $note_body = 'Unknown note type';
                 switch ( $comment_content['note_type'] )
@@ -104,32 +118,60 @@ class PH_Meta_Box_Contact_Notes {
                         break;
                     }
                 }
+
+                $note_output[] = array(
+                    'id' => $note->comment_ID,
+                    'post_id' => $note->comment_post_ID,
+                    'type' => $comment_content['note_type'],
+                    'author' => $note->comment_author,
+                    'body' => $note_body,
+                    'timestamp' => strtotime($note->comment_date),
+                    'internal' => true
+                );
+            }
+        }
+
+        $note_output = apply_filters( 'propertyhive_notes', $note_output, $post );
+        $note_output = apply_filters( 'propertyhive_contact_notes', $note_output, $post );
+
+        if ( !empty($note_output) )
+        {
+            // order by date desc
+            $timestamp = array_column($note_output, 'timestamp');
+
+            array_multisort($timestamp, SORT_DESC, $note_output);
+
+            foreach ( $note_output as $note )
+            {
+                $note_classes = array( 'note' );
+
+                $note_classes[] = 'note-type-' . $note['type'];
 ?>
-                <li rel="<?php echo absint( $note->comment_ID ) ; ?>" class="<?php echo implode( ' ', $note_classes ); ?>">
+                <li rel="<?php echo absint( $note['id'] ) ; ?>" class="<?php echo implode( ' ', $note_classes ); ?>">
                     <div class="note_content">
-                        <?php echo wp_kses_post( $note_body ); ?>
+                        <?php echo wp_kses_post( $note['body'] ); ?>
                     </div>
                     <p class="meta">
-                        <abbr class="exact-date" title="<?php echo $note->comment_date_gmt; ?> GMT">
+                        <abbr class="exact-date" title="<?php echo date("Y-m-d H:i:s", $note['timestamp']); ?>">
                             <?php 
                                 
-                                $time_diff =  current_time( 'timestamp', 1 ) - strtotime( $note->comment_date_gmt );
+                                $time_diff =  current_time( 'timestamp', 1 ) - $note['timestamp'];
 
                                 if ($time_diff > 86400) {
-                                    echo date( $datetime_format, strtotime( $note->comment_date_gmt ) );
+                                    echo date( $datetime_format, $note['timestamp'] );
                                 } else {
-                                    printf( __( '%s ago', 'propertyhive' ), human_time_diff( strtotime( $note->comment_date_gmt ), current_time( 'timestamp', 1 ) ) );
+                                    printf( __( '%s ago', 'propertyhive' ), human_time_diff( $note['timestamp'], current_time( 'timestamp', 1 ) ) );
                                 }
                             ?>
                         </abbr>
-                        <?php if ( $note->comment_author !== __( 'Property Hive', 'propertyhive' ) ) printf( ' ' . __( 'by %s', 'propertyhive' ), $note->comment_author ); ?>
-                        <?php if ($comment_content['note_type'] == 'note') { ?><a href="#" class="delete_note"><?php _e( 'Delete', 'propertyhive' ); ?></a><?php } ?>
+                        <?php if ( $note['author'] !== __( 'Property Hive', 'propertyhive' ) && $note['author'] != '' ) printf( ' ' . __( 'by %s', 'propertyhive' ), $note['author'] ); ?>
+                        <?php if ( $note['type'] == 'note' ) { ?><a href="#" class="delete_note"><?php _e( 'Delete', 'propertyhive' ); ?></a><?php } ?>
                         <?php
-                            if ( $post->ID != $note->comment_post_ID )
+                            if ( $post->ID != $note['post_id'] )
                             {
                         ?>
                         <br>
-                        <?php echo __( 'Note originally entered on', 'propertyhive' ); ?> <a href="<?php echo get_edit_post_link($note->comment_post_ID); ?>" style="color:inherit;"><?php echo __( ucfirst(get_post_type($note->comment_post_ID)), 'propertyhive' ); ?></a>
+                        <?php echo __( 'Note originally entered on', 'propertyhive' ); ?> <a href="<?php echo get_edit_post_link($note['post_id']); ?>" style="color:inherit;"><?php echo __( ucfirst(get_post_type($note['post_id'])), 'propertyhive' ); ?></a>
                         <?php
                             }
                         ?>
@@ -139,7 +181,7 @@ class PH_Meta_Box_Contact_Notes {
             }
         }
 
-        echo '<li id="no_notes" style="text-align:center;' . ( (!empty($notes)) ? 'display:none;' : '' ) . '">' . __( 'There are no notes to display', 'propertyhive' ) . '</li>';
+        echo '<li id="no_notes" style="text-align:center;' . ( (!empty($note_output)) ? 'display:none;' : '' ) . '">' . __( 'There are no notes to display', 'propertyhive' ) . '</li>';
 
         echo '</ul>';
         
