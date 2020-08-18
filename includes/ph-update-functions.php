c8ddc3a216597a7963f256fdb5e9a41ed7fc9e88
Eoin Garvey <67641630+egar01n@users.noreply.github.com>
2020-08-18 12:14:27 +0100

    Set up system to allow DB updates to be run on plugin update  - Also add first function to add on_market_change_date meta_keys

---
+++ includes/ph-update-functions.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * PropertyHive Updates
+ *
+ * Functions for updating data during an update.
+ *
+ * @author      PropertyHive
+ * @category    Core
+ * @package     PropertyHive/Functions
+ * @version     1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Update on_market_change_dates for 1.4.58
+ *
+ * @return void
+ */
+function propertyhive_update_1458_on_market_change_dates() {
+    global $wpdb;
+
+    $args = array(
+        'post_type' => 'property',
+        'fields' => 'ids',
+        'post_status' => 'publish',
+        'meta_query' => array(
+            array(
+                'key' => '_on_market',
+                'value' => 'yes'
+            ),
+            array(
+                'key' => '_on_market_change_date',
+                'compare' => 'NOT EXISTS'
+            )
+        ),
+        'nopaging' => true,
+    );
+    $property_query =  new WP_Query($args);
+
+    if ( $property_query->have_posts() )
+    {
+        while ( $property_query->have_posts() )
+        {
+            $property_query->the_post();
+
+            $date_post_written = get_the_date();
+            add_post_meta( get_the_id(), '_on_market_change_date', $date_post_written );
+        }
+    }
+
+    wp_reset_postdata();
+}
