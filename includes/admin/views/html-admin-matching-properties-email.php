36a3afcb9872c0e25f9b5fab72535cb8d5bfd11f
Property Hive <info@wp-property-hive.com>
2020-10-22 22:10:56 +0100

    Add new Cc and Bcc fields/support to match processes

--- includes/admin/views/html-admin-matching-properties-email.php
+++ includes/admin/views/html-admin-matching-properties-email.php
@@ -5,7 +5,22 @@
     <tr valign="top">
         <th scope="row" class="titledesc"><?php echo __( 'To', 'propertyhive' ); ?></th>
         <td class="forminp">
-            <input type="text" name="to_email_address" value="<?php echo get_post_meta( $contact_id, '_email_address', TRUE ); ?>" style="width:100%;">
+            <input type="text" name="to_email_address" value="<?php echo get_post_meta( $contact_id, '_email_address', TRUE ); ?>" style="width:100%; margin-bottom:5px;">
+            <a href="" class="show-cc">Show Cc</a> &nbsp;|&nbsp; <a href="" class="show-bcc">Show Bcc</a>
+        </td>
+    </tr>
+
+    <tr valign="top" style="display:none" id="cc_email_address_row">
+        <th scope="row" class="titledesc"><?php echo __( 'Cc', 'propertyhive' ); ?></th>
+        <td class="forminp">
+            <input type="text" name="cc_email_address" value="" style="width:100%;">
+        </td>
+    </tr>
+
+    <tr valign="top" style="display:none" id="bcc_email_address_row">
+        <th scope="row" class="titledesc"><?php echo __( 'Bcc', 'propertyhive' ); ?></th>
+        <td class="forminp">
+            <input type="text" name="bcc_email_address" value="" style="width:100%;">
         </td>
     </tr>
 
@@ -42,3 +57,26 @@
     </tr>
 
 </table>
+
+<script>
+
+jQuery(document).ready(function()
+{
+    jQuery('a.show-cc').click(function(e)
+    {
+        e.preventDefault();
+
+        jQuery('#cc_email_address_row').fadeIn('fast');
+        jQuery('#cc_email_address_row input').focus();
+    });
+
+    jQuery('a.show-bcc').click(function(e)
+    {
+        e.preventDefault();
+
+        jQuery('#bcc_email_address_row').fadeIn('fast');
+        jQuery('#bcc_email_address_row input').focus();
+    });
+});
+
+</script>
