7ecf5bf72187db8c125449cc880d27e671ebfa99
Property Hive <info@wp-property-hive.com>
2020-11-26 14:04:08 +0000

    Add abilty to search contacts in backend by address

--- includes/admin/class-ph-admin-post-types.php
+++ includes/admin/class-ph-admin-post-types.php
@@ -1197,7 +1197,21 @@ class PH_Admin_Post_Types {
         if ( !isset($_GET['s']) || ( isset($_GET['s']) && ph_clean($_GET['s']) == '' ) )
             return $join;
 
-        if ( 'appraisal' === $typenow ) 
+        if ( 'contact' === $typenow ) 
+        {
+            $join .= " 
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_company_name ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_company_name.post_id AND ph_contact_filter_meta_company_name.meta_key = '_company_name'
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_name_number ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_name_number.post_id AND ph_contact_filter_meta_name_number.meta_key = '_address_name_number'
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_street ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_street.post_id AND ph_contact_filter_meta_street.meta_key = '_address_street'
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_2 ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_2.post_id AND ph_contact_filter_meta_2.meta_key = '_address_two'
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_3 ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_3.post_id AND ph_contact_filter_meta_3.meta_key = '_address_three'
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_4 ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_4.post_id AND ph_contact_filter_meta_4.meta_key = '_address_four'
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_postcode ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_postcode.post_id AND ph_contact_filter_meta_postcode.meta_key = '_address_postcode'
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_email_address ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_email_address.post_id AND ph_contact_filter_meta_email_address.meta_key = '_email_address'
+LEFT JOIN " . $wpdb->postmeta . " AS ph_contact_filter_meta_telephone_number ON " . $wpdb->posts . ".ID = ph_contact_filter_meta_telephone_number.post_id AND ph_contact_filter_meta_telephone_number.meta_key = '_telephone_number_clean'
+";
+        }
+        elseif ( 'appraisal' === $typenow ) 
         {
             $join .= " 
 LEFT JOIN " . $wpdb->postmeta . " AS ph_appraisal_filter_meta_name_number ON " . $wpdb->posts . ".ID = ph_appraisal_filter_meta_name_number.post_id AND ph_appraisal_filter_meta_name_number.meta_key = '_address_name_number'
@@ -1234,7 +1248,36 @@ LEFT JOIN " . $wpdb->posts . " AS ph_applicant_filter_posts ON ph_applicant_filt
         if ( !isset($_GET['s']) || ( isset($_GET['s']) && ph_clean($_GET['s']) == '' ) )
             return $where;
 
-        if ( 'appraisal' === $typenow ) 
+        if ( 'contact' === $typenow ) 
+        {
+            $phone_number = preg_replace( "/[^0-9,]/", "", ph_clean($_GET['s']) );
+
+            $where = preg_replace(
+                "/\(\s*" . $wpdb->posts . ".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+                "(
+                    (" . $wpdb->posts . ".post_title LIKE $1) 
+                    OR
+                    (ph_contact_filter_meta_company_name.meta_value LIKE $1)
+                    OR 
+                    (ph_contact_filter_meta_name_number.meta_value LIKE $1)
+                    OR 
+                    (ph_contact_filter_meta_street.meta_value LIKE $1)
+                    OR 
+                    (ph_contact_filter_meta_2.meta_value LIKE $1)
+                    OR 
+                    (ph_contact_filter_meta_3.meta_value LIKE $1)
+                    OR 
+                    (ph_contact_filter_meta_4.meta_value LIKE $1)
+                    OR 
+                    (ph_contact_filter_meta_postcode.meta_value LIKE $1)
+                    OR 
+                    (ph_contact_filter_meta_email_address.meta_value LIKE $1)
+                    " . ( $phone_number != '' ? "OR (ph_contact_filter_meta_telephone_number.meta_value LIKE '%" . $phone_number . "%')" : '' ) . "
+                )", 
+                $where 
+            );
+        }
+        elseif ( 'appraisal' === $typenow ) 
         {
             $where = preg_replace(
                 "/\(\s*" . $wpdb->posts . ".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
