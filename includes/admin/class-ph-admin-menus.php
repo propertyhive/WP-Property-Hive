f103817aac3c9a288dde00e476a72005b674dbd6
Kit Pitcairn <KitPitcairn@gmail.com>
2020-12-01 11:35:05 +0000

    Add a Tenancies Module

--- includes/admin/class-ph-admin-menus.php
+++ includes/admin/class-ph-admin-menus.php
@@ -94,6 +94,11 @@ class PH_Admin_Menus {
 	        add_submenu_page( 'propertyhive', __( 'Sales', 'propertyhive' ), __( 'Sales', 'propertyhive' ), 'manage_propertyhive', 'edit.php?post_type=sale'/*, array( $this, 'attributes_page' )*/ );
 	    }
 
+	    if ( get_option('propertyhive_module_disabled_tenancies', '') != 'yes' )
+	    {
+	        add_submenu_page( 'propertyhive', __( 'Tenancies', 'propertyhive' ), __( 'Tenancies', 'propertyhive' ), 'manage_propertyhive', 'edit.php?post_type=tenancy'/*, array( $this, 'attributes_page' )*/ );
+	    }
+
     	if ( get_option('propertyhive_module_disabled_contacts', '') != 'yes' )
 	    {
 	        add_submenu_page( null, __( 'Applicant Matching Properties', 'propertyhive'), __( 'Applicant Matching Properties', 'propertyhive' ), 'manage_propertyhive', 'ph-matching-properties', array($this, 'matching_properties_page'));
