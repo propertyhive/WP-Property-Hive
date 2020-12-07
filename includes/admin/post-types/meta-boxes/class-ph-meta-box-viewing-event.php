5b71a216d27d6b16a372fbf6b084daddef48eb7c
Property Hive <info@wp-property-hive.com>
2020-12-07 09:39:16 +0000

    Add filter to negotiator drodowns to control roles excluded

--- includes/admin/post-types/meta-boxes/class-ph-meta-box-viewing-event.php
+++ includes/admin/post-types/meta-boxes/class-ph-meta-box-viewing-event.php
@@ -141,7 +141,7 @@ class PH_Meta_Box_Viewing_Event {
         $args = array(
             'number' => 9999,
             'orderby' => 'display_name',
-            'role__not_in' => array('property_hive_contact', 'subscriber') 
+            'role__not_in' => apply_filters( 'property_negotiator_exclude_roles', array('property_hive_contact', 'subscriber') )
         );
         $user_query = new WP_User_Query( $args );
 
