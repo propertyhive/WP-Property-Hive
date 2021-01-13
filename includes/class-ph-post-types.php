2a0845476282fb5b01c041849726181e37d97622
Kit Pitcairn <KitPitcairn@gmail.com>
2021-01-13 13:17:33 +0000

    Register key_date post type

--- includes/class-ph-post-types.php
+++ includes/class-ph-post-types.php
@@ -546,7 +546,40 @@ class PH_Post_types {
                 )
             )
         );
-        
+
+		register_post_type( "key_date",
+			apply_filters( 'propertyhive_register_post_type_key_date',
+				array(
+					'labels' => array(
+						'name'                  => __( 'Key Dates', 'propertyhive' ),
+						'singular_name'         => __( 'Key Date', 'propertyhive' ),
+						'menu_name'             => _x( 'Management', 'Admin menu name', 'propertyhive' ),
+						'add_new'               => __( 'Add Key Date', 'propertyhive' ),
+						'add_new_item'          => __( 'Add New Key Date', 'propertyhive' ),
+						'edit'                  => __( 'Edit', 'propertyhive' ),
+						'edit_item'             => __( 'Edit Key Date', 'propertyhive' ),
+						'new_item'              => __( 'New Key Date', 'propertyhive' ),
+						'view'                  => __( 'View Key Date', 'propertyhive' ),
+						'view_item'             => __( 'View Key Date', 'propertyhive' ),
+						'search_items'          => __( 'Search Key Dates', 'propertyhive' ),
+						'not_found'             => __( 'No key dates found', 'propertyhive' ),
+						'not_found_in_trash'    => __( 'No key dates found in trash', 'propertyhive' ),
+						'parent'                => __( 'Parent Key Date', 'propertyhive' )
+					),
+					'public'                => false,
+					'show_ui'               => true,
+					'capability_type'       => 'post',
+					'map_meta_cap'          => true,
+					'publicly_queryable'    => false,
+					'exclude_from_search'   => true,
+					'hierarchical'          => false, // Hierarchical causes memory issues - WP loads all records!
+					'query_var'             => true,
+					'supports'              => false,
+					'show_in_nav_menus'     => false,
+					'show_in_menu'          => false
+				)
+			)
+		);
         do_action( 'propertyhive_after_register_post_types' );
 	}
 
