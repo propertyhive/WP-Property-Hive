0010f18c45fd6409df1146a3bebe661350e3da91
Eoin Garvey <67641630+egar01n@users.noreply.github.com>
2021-02-09 18:07:25 +0000

    Improvements to admin Key Dates list:  - Move Update button to be next to Cancel  - Rename Type heading to Description  - Include Booked viewings in Overdue filter  - When viewing Pending, include Overdue as well, if they're Pending  - Make Date Type filter work

--- assets/js/admin/inline-edit-key_date.js
+++ assets/js/admin/inline-edit-key_date.js
@@ -13,14 +13,18 @@ jQuery(function($) {
         if ( id > 0 ) {
 
             var specific_post_edit_row = $( '#edit-' + id ),
+                quick_edit_save_button = $( 'button.save', specific_post_edit_row ),
                 specific_post_row = $( '#post-' + id ),
                 description = $( '.column-description .cell-main-content', specific_post_row ).text(),
                 property = $( '.column-property .cell-main-content', specific_post_row ).text(),
                 display_status = $( '.column-status .cell-main-content', specific_post_row ).text(),
                 real_status = ["pending", "upcoming", "overdue"].includes(display_status.toLowerCase()) ? 'pending' : display_status.toLowerCase();
 
+            // Replace classes so quick edit Update button is next to Cancel
+            quick_edit_save_button.addClass('alignmiddle').removeClass('alignright').before('&nbsp;');
+
             $( ':input[name="_key_date_status"] option[value="' + real_status + '"]', specific_post_edit_row ).attr("selected", "selected").text(display_status);
-            $( '.key_date-type', specific_post_edit_row ).text(description);
+            $( '.key_date-description', specific_post_edit_row ).text(description);
             $( '.key_date-property', specific_post_edit_row ).text(property);
         }
     }
