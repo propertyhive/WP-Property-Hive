8066d4b903ab6d4ae6822fea0dcabbb3968e17fd
Steve Marks <info@wp-property-hive.com>
2021-03-04 06:05:57 +0000

    Add property, owner and applicant details to viewing details lightbox

--- includes/admin/views/html-lightbox-property-details.php
+++ includes/admin/views/html-lightbox-property-details.php
@@ -1,1 +1,21 @@
-PROPERTY DETAILS IN VIEW <?php echo $property_id; ?>
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+$property = new PH_Property((int)$property_id);
+
+?>
+
+<div class="thumbnail">
+	<img src="<?php echo $property->get_main_photo_src(); ?>" alt="">
+</div>
+
+<div class="details">
+
+	<div class="address"><a href="<?php echo get_edit_post_link((int)$property_id); ?>"><?php echo $property->get_formatted_summary_address(); ?></a></div>
+
+	<div class="price"><?php echo $property->get_formatted_price(); ?></div>
+
+</div>
+
+<div style="clear:both"></div>
