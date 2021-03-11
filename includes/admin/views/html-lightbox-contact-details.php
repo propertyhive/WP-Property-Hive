90e0a9184599712a278928a6c32d3cf2aaabdca3
Steve Marks <info@wp-property-hive.com>
2021-03-11 10:43:22 +0000

    Added contact address to lightbox details

--- includes/admin/views/html-lightbox-contact-details.php
+++ includes/admin/views/html-lightbox-contact-details.php
@@ -15,10 +15,12 @@ if ( !empty($contact_ids) )
 {
 	foreach ( $contact_ids as $contact_id )
 	{
+		$contact = new PH_Contact((int)$contact_id);
 ?>
 <div class="contact">
 	<div class="name"><a href="<?php echo get_edit_post_link((int)$contact_id); ?>"><?php echo get_the_title((int)$contact_id); ?></a></div>
 	<div class="contact-details">
+		<?php if ( $contact->get_formatted_full_address() != '' ) { echo $contact->get_formatted_full_address() . '<br>'; } ?>
 		T: <?php echo get_post_meta((int)$contact_id, '_telephone_number', TRUE); ?><br>
 		E: <?php echo get_post_meta((int)$contact_id, '_email_address', TRUE); ?>
 	</div>
