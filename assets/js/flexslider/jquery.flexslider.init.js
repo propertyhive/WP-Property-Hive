5aaf0a8b0c19d274b564c0b52460bbe68be82302
Steve Marks <info@wp-property-hive.com>
2021-05-17 08:07:34 +0100

    Ensure thumbnail heights are consistent and work when lazyloading in effect

--- assets/js/flexslider/jquery.flexslider.init.js
+++ assets/js/flexslider/jquery.flexslider.init.js
@@ -3,8 +3,21 @@ jQuery(window).on('load', function() {
 });
 
 // Resize flexsider image to prevent images showing as incorrect height when lazy loading
-jQuery('#slider img').on('load',function(){
-    jQuery(window).trigger('resize');
+jQuery('#slider.flexslider .slides img').on('load', function(){
+    setTimeout(function() { jQuery(window).trigger('resize'); }, 500);
+});
+jQuery('#carousel.thumbnails .slides img').on('load', function(){
+    setTimeout(function() { jQuery(window).trigger('resize'); }, 500);
+});
+
+jQuery(window).on('resize', function() {
+    // set height of all thumbnails to be the same (i.e. height of the first one) 
+    jQuery('#carousel.thumbnails .slides img').css('height', 'auto');
+    var thumbnail_height = jQuery('#carousel.thumbnails .slides img:eq(0)').height();
+    jQuery('#carousel.thumbnails .slides img').each(function()
+    {
+        jQuery(this).height(thumbnail_height);
+    });
 });
 
 function ph_init_slideshow()
