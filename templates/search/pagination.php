8d4cd599e88df82738b07663b2999466a7fb6cdd
Eoin Garvey <67641630+egar01n@users.noreply.github.com>
2021-06-08 15:56:38 +0100

    Add pagination attribute to properties shortcodes

--- templates/search/pagination.php
+++ templates/search/pagination.php
@@ -9,17 +9,16 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-global $wp_query;
 ?>
 <div class="propertyhive-pagination">
 	<?php
-		if ( $wp_query->max_num_pages  > 1 )
+		if ( $max_num_pages  > 1 )
 		{
 			echo paginate_links( apply_filters( 'propertyhive_pagination_args', array(
 				'base'         => esc_url_raw( str_replace( 999999999, '%#%', get_pagenum_link( 999999999, false ) ) ),
 				'format'       => '',
 				'current'      => max( 1, get_query_var( 'paged' ) ),
-				'total'        => $wp_query->max_num_pages,
+				'total'        => $max_num_pages,
 				'prev_text'    => '&larr;',
 				'next_text'    => '&rarr;',
 				'type'         => 'list',
