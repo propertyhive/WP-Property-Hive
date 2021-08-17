7a2d8cafa1a77a25c05455ca61713428735d50ca
Steve Marks <info@wp-property-hive.com>
2021-08-17 09:03:45 +0100

    New meta function to output file upload field

--- includes/admin/ph-meta-box-functions.php
+++ includes/admin/ph-meta-box-functions.php
@@ -65,7 +65,7 @@ function propertyhive_wp_text_input( $field ) {
 }
 
 /**
- * Output a file input box.
+ * Output a photo input box.
  *
  * @access public
  * @param array $field
@@ -166,13 +166,129 @@ function propertyhive_wp_photo_upload( $field ) {
 	                    attachment = attachment.toJSON();
 	             
 	                    // Do something with attachment.id and/or attachment.url here
-	                    console.log(attachment.url);
+	                    console.log(attachment);
 	                    
-	                    // Add selected image to page
-	                    //add_photo_attachment_to_grid(attachment);
-
 	                    jQuery(\'.form-field.' . esc_attr( $field['id'] ) . '_field\').show();
 	                    jQuery(\'.form-field.' . esc_attr( $field['id'] ) . '_field span\').html(\'<img src="\' + attachment.url + \'" width="150" alt="">\');
+	                    jQuery(\'#' . esc_attr( $field['id'] ) . '\').val(attachment.id);
+	                });
+	            });
+	         
+	            // Finally, open the modal
+	            file_frame' . $field['id'] . '.open();
+	        });
+		});
+
+	</script>';
+}
+
+/**
+ * Output a file input box.
+ *
+ * @access public
+ * @param array $field
+ * @return void
+ */
+function propertyhive_wp_file_upload( $field ) {
+	global $thepostid, $post, $propertyhive;
+
+	$thepostid              = empty( $thepostid ) ? $post->ID : $thepostid;
+	$field['id']  			= isset( $field['id'] ) ? $field['id'] : '';
+	$field['button_label']  = isset( $field['button_label'] ) ? $field['button_label'] : __('Select Photo', 'propertyhive');
+	$field['class']         = isset( $field['class'] ) ? $field['class'] : 'short';
+	$field['wrapper_class'] = isset( $field['wrapper_class'] ) ? $field['wrapper_class'] : '';
+	$field['value']         = isset( $field['value'] ) ? $field['value'] : get_post_meta( $thepostid, $field['id'], true );
+	$field['name']          = isset( $field['name'] ) ? $field['name'] : $field['id'];
+	$data_type              = empty( $field['data_type'] ) ? '' : $field['data_type'];
+
+	// Custom attribute handling
+	$custom_attributes = array();
+
+	if ( ! empty( $field['custom_attributes'] ) && is_array( $field['custom_attributes'] ) )
+		foreach ( $field['custom_attributes'] as $attribute => $value )
+			$custom_attributes[] = esc_attr( $attribute ) . '="' . esc_attr( $value ) . '"';
+
+	propertyhive_wp_hidden_input( $field );
+
+	echo '
+	<p class="form-field ' . esc_attr( $field['id'] ) . '_field ' . esc_attr( $field['wrapper_class'] ) . '"';
+	if ( $field['value'] == '' )
+	{
+		echo ' style="display:none;"';
+	}
+	echo '>
+	   <label for="' . esc_attr( $field['id'] ) . '">' . __( 'Uploaded', 'propertyhive' ) . ' ' . wp_kses_post( $field['label'] ) . '</label>
+	   <span>';
+	if ( $field['value'] != '' )
+	{
+		$file = get_attached_file( $field['value'] );
+		if ( $file !== FALSE )
+		{
+			$filename = basename( $file );
+			echo '<a href="' . wp_get_attachment_url($field['value']) . '" target="_blank">' . $filename . '</a>';
+		}
+		else
+		{
+			echo 'Image doesn\'t exist';
+		}
+	}
+	echo '</span>
+	</p>';
+
+	echo '
+	<p class="form-field ' . esc_attr( $field['id'] ) . '_field ' . esc_attr( $field['wrapper_class'] ) . '">
+	   <label for="' . esc_attr( $field['id'] ) . '">' . wp_kses_post( $field['label'] ) . '</label>
+	   <a href="" class="button button-primary ph_upload_file_button' . $field['id'] . '">' . $field['button_label'] . '</a>';
+
+	if ( ! empty( $field['description'] ) ) {
+
+		if ( isset( $field['desc_tip'] ) && false !== $field['desc_tip'] ) {
+			echo '<img class="help_tip" data-tip="' . esc_attr( $field['description'] ) . '" src="' . esc_url( PH()->plugin_url() ) . '/assets/images/help.png" height="16" width="16" />';
+		} else {
+			echo '<span class="description">' . wp_kses_post( $field['description'] ) . '</span>';
+		}
+
+	}
+	echo '</p>';
+
+	echo '<script>
+
+		var file_frame' . $field['id'] . ';
+
+		jQuery(document).ready(function()
+        {
+        	jQuery(\'body\').on(\'click\', \'.ph_upload_file_button' . $field['id'] . '\', function( event ){
+                 
+	            event.preventDefault();
+	         
+	            // If the media frame already exists, reopen it.
+	            if ( file_frame' . $field['id'] . ' ) {
+	              file_frame' . $field['id'] . '.open();
+	              return;
+	            }
+	         
+	            // Create the media frame.
+	            file_frame' . $field['id'] . ' = wp.media.frames.file_frame' . $field['id'] . ' = wp.media({
+	              title: jQuery( this ).data( \'uploader_title\' ),
+	              button: {
+	                text: jQuery( this ).data( \'uploader_button_text\' ),
+	              },
+	              multiple: false  // Set to true to allow multiple files to be selected
+	            });
+	         
+	            // When an image is selected, run a callback.
+	            file_frame' . $field['id'] . '.on( \'select\', function() {
+	                var selection = file_frame' . $field['id'] . '.state().get(\'selection\');
+
+	                selection.map( function( attachment ) {
+	             
+	                    attachment = attachment.toJSON();
+	             
+	                    // Do something with attachment.id and/or attachment.url here
+	                    console.log(attachment);
+	                    
+	                    jQuery(\'.form-field.' . esc_attr( $field['id'] ) . '_field\').show();
+	                    jQuery(\'.form-field.' . esc_attr( $field['id'] ) . '_field span\').html(\'<a href="\' + attachment.url + \'" target="_blank">\' + attachment.filename + \'</a>\');
 	                    jQuery(\'#' . esc_attr( $field['id'] ) . '\').val(attachment.id);
 	                });
 	            });
