3d4dd7febb2b1968bd80d860e7fcf6c237f3a4b7
Steve Marks <info@wp-property-hive.com>
2022-05-17 11:43:30 +0100

    Added new Elementor reference number widget

---
+++ includes/elementor-widgets/property-reference-number.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Elementor Property Reference Number Widget.
+ *
+ * @since 1.0.0
+ */
+class Elementor_Property_Reference_Number_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'property-reference-number';
+	}
+
+	public function get_title() {
+		return __( 'Reference Number', 'propertyhive' );
+	}
+
+	public function get_icon() {
+		return 'fa fa-i-cursor';
+	}
+
+	public function get_categories() {
+		return [ 'property-hive' ];
+	}
+
+	public function get_keywords() {
+		return [ 'property hive', 'propertyhive', 'property', 'reference' ];
+	}
+
+	protected function _register_controls() {
+
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => __( 'Reference Number', 'propertyhive' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'before',
+			[
+				'label' => __( 'Before', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => __( 'Ref', 'propertyhive' ),
+			]
+		);
+
+		$this->add_control(
+			'after',
+			[
+				'label' => __( 'After', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => __( 'Reference Number', 'propertyhive' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'typography',
+				'label' => __( 'Typography', 'propertyhive' ),
+				'scheme' => \Elementor\Core\Schemes\Typography::TYPOGRAPHY_1,
+				'selector' => '{{WRAPPER}} .elementor-widget-reference-number',
+			]
+		);
+
+		$this->add_control(
+			'color',
+			[
+				'label' => __( 'Colour', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => \Elementor\Core\Schemes\Color::get_type(),
+					'value' => \Elementor\Core\Schemes\Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-widget-reference-number' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	protected function render() {
+
+		global $property;
+
+		$settings = $this->get_settings_for_display();
+
+		if ( !isset($property->id) ) {
+			return;
+		}
+
+		if ( $property->reference_number != '' )
+		{
+	        echo '<div class="elementor-widget-reference-number">';
+	        if ( isset($settings['before']) && !empty($settings['before']) )
+	        {
+	        	echo $settings['before'] . ' ';
+	        }
+	        echo $property->reference_number;
+	        if ( isset($settings['after']) && !empty($settings['after']) )
+	        {
+	        	echo ' ' . $settings['after'];
+	        }
+	        echo '</div>';
+	    }
+
+	}
+
+}
