b08a7eb66a417d147cd9dbdc81d5de52688f63fd
Steve Marks <info@wp-property-hive.com>
2022-11-07 17:16:22 +0000

    Update to version 1.5.40 and updated changelog

--- propertyhive.php
+++ propertyhive.php
@@ -3,11 +3,11 @@
  * Plugin Name: Property Hive
  * Plugin URI: https://wordpress.org/plugins/propertyhive/
  * Description: Estate Agency Property Software Plugin for WordPress
- * Version: 1.5.39
+ * Version: 1.5.40
  * Author: PropertyHive
  * Author URI: https://wp-property-hive.com
  * Requires at least: 3.8
- * Tested up to: 6.0.2
+ * Tested up to: 6.1
  * 
  * Text Domain: propertyhive
  * Domain Path: /i18n/languages/
@@ -27,14 +27,14 @@ if ( ! class_exists( 'PropertyHive' ) )
     * Main PropertyHive Class
     *
     * @class PropertyHive
-    * @version 1.5.39
+    * @version 1.5.40
     */
     final class PropertyHive {
          
         /**
          * @var string
          */
-        public $version = '1.5.39';
+        public $version = '1.5.40';
          
         /**
          * @var PropertyHive The single instance of the class
