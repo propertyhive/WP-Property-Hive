292b9db7f516a843fc6d7ee660efc46224115af7
Steve Marks <info@wp-property-hive.com>
2022-12-06 15:39:13 +0000

    Tweaks to settings tab class names, adding unique class per tab

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -1,9 +1,9 @@
 <div class="wrap propertyhive">
 	<form method="post" id="mainform" action="" enctype="multipart/form-data">
-		<div class="icon32 icon32-propertyhive-settings" id="icon-propertyhive"><br /></div><h2 class="nav-tab-wrapper woo-nav-tab-wrapper">
+		<div class="icon32 icon32-propertyhive-settings" id="icon-propertyhive"><br /></div><h2 class="nav-tab-wrapper">
 			<?php
 				foreach ( $tabs as $name => $label )
-					echo '<a href="' . admin_url( 'admin.php?page=ph-settings&tab=' . $name ) . '" class="nav-tab ' . ( $current_tab == $name ? 'nav-tab-active' : '' ) . '">' . $label . '</a>';
+					echo '<a href="' . admin_url( 'admin.php?page=ph-settings&tab=' . $name ) . '" class="nav-tab nav-tab-' . sanitize_title($name) . ' ' . ( $current_tab == $name ? 'nav-tab-active' : '' ) . '">' . $label . '</a>';
 
 				do_action( 'propertyhive_settings_tabs' );
 			?>
