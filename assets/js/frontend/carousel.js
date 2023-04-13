14d8ff108108876326cef832b0f72ce8798dc628
Steve Marks <info@wp-property-hive.com>
2023-04-13 07:46:48 +0100

    Support for multiple shortcode carousels on one page

--- assets/js/frontend/carousel.js
+++ assets/js/frontend/carousel.js
@@ -1,13 +1,15 @@
 jQuery(window).load(function()
 {
-	var slider = tns({
-    	container: '.propertyhive-shortcode-carousel',
-    	items: propertyhive_carousel_params.items,
-    	controlsPosition: propertyhive_carousel_params.controlsPosition,
-    	nav: false,
-    	gutter: propertyhive_carousel_params.gutter,
-    	mouseDrag: propertyhive_carousel_params.mouseDrag,
-    	controlsText: propertyhive_carousel_params.controlsText,
-    	responsive: propertyhive_carousel_params.responsive
-  	});
+  	document.querySelectorAll('.propertyhive-shortcode-carousel').forEach(slider => {
+	    tns({
+	        container: slider,
+	        items: propertyhive_carousel_params.items,
+	    	controlsPosition: propertyhive_carousel_params.controlsPosition,
+	    	nav: false,
+	    	gutter: propertyhive_carousel_params.gutter,
+	    	mouseDrag: propertyhive_carousel_params.mouseDrag,
+	    	controlsText: propertyhive_carousel_params.controlsText,
+	    	responsive: propertyhive_carousel_params.responsive
+	    });
+	});
 });
