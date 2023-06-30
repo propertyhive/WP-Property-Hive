d78ad6c3e56d1efa58e74aff9a66f85f06c78481
Martin-PH <martineli.works@gmail.com>
2023-06-30 10:18:38 +0100

    Display Maps as Link(via Fancybox)- Elementor widget.

---
+++ includes/elementor-widgets/property-maps-link.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Elementor Property Maps Link Widget.
+ *
+ * @since 1.0.0
+ */
+class Elementor_Property_Maps_Link_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'property-maps-link';
+	}
+
+	public function get_title() {
+		return __( 'Maps Link', 'propertyhive' );
+	}
+
+	public function get_icon() {
+		return 'eicon-document-file';
+	}
+
+	public function get_categories() {
+		return [ 'property-hive' ];
+	}
+
+	public function get_keywords() {
+		return [ 'property hive', 'propertyhive', 'property', 'map', 'location', 'google map' ];
+	}
+
+	protected function register_controls() {
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => __( 'Maps Link Settings', 'propertyhive' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'typography',
+				'label' => __( 'Typography', 'propertyhive' ),
+				'scheme' => \Elementor\Core\Schemes\Typography::TYPOGRAPHY_1,
+				'selector' => '{{WRAPPER}} a',
+			]
+		);
+
+		$this->add_control(
+			'color',
+			[
+				'label' => __( 'Colour', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => \Elementor\Core\Schemes\Color::get_type(),
+					'value' => \Elementor\Core\Schemes\Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} a' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'text_align',
+			[
+				'label' => __( 'Alignment', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => __( 'Left', 'propertyhive' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'propertyhive' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', 'propertyhive' ),
+						'icon' => 'eicon-text-align-right',
+					],
+				],
+				'default' => 'center',
+				'toggle' => true,
+				'selectors' => [
+					'{{WRAPPER}}' => 'text-align: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'background_color',
+			[
+				'label' => __( 'Background Colour', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => \Elementor\Core\Schemes\Color::get_type(),
+					'value' => \Elementor\Core\Schemes\Color::COLOR_2,
+				],
+				'selectors' => [
+					'{{WRAPPER}} a' => 'background: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'padding',
+			[
+				'label' => __( 'Link Padding', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px' ],
+				'selectors' => [
+					'{{WRAPPER}} a' => 'display:inline-block; padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+				'default' => [
+					'top' => 5,
+					'right' => 5,
+					'bottom' => 5,
+					'left' => 5,
+					'isLinked' => true,
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	protected function render() {
+
+		global $property;
+
+		$settings = $this->get_settings_for_display();
+
+		if ( !isset($property->id) ) {
+			return;
+		}
+		echo '<a href = "#map_lightbox" data-fancybox> View Map </a>';
+		
+	echo '<div id="map_lightbox" style="display:none; width:90%; max-width:600px;">';
+   	 echo do_shortcode('[property_map]');
+    	echo '</div>';
+
+
+	}
+}
