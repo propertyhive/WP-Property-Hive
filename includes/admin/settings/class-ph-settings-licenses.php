d6165219179b987f2fd8dd743341d8d0c054440e
Steve Marks <info@wp-property-hive.com>
2023-07-21 09:20:46 +0100

    Move features to their own tab

--- includes/admin/settings/class-ph-settings-licenses.php
+++ includes/admin/settings/class-ph-settings-licenses.php
@@ -27,50 +27,16 @@ class PH_Settings_Licenses extends PH_Settings_Page {
 		$this->label = __( 'PRO / License Key', 'propertyhive' );
 
 		add_filter( 'propertyhive_settings_tabs_array', array( $this, 'add_settings_page' ), 20 );
-		add_action( 'propertyhive_sections_' . $this->id, array( $this, 'output_sections' ) );
 		add_action( 'propertyhive_settings_' . $this->id, array( $this, 'output' ) );
 		add_action( 'propertyhive_settings_save_' . $this->id, array( $this, 'save' ) );
-		add_action( 'propertyhive_admin_field_pro_features', array( $this, 'pro_features_setting' ) );
 	}
-
-	/**
-	 * Get sections.
-	 *
-	 * @return array
-	 */
-	public function get_sections() {
-		$sections = array(
-			'' => __( 'Activated Features', 'propertyhive' ),
-			'licensekey' => __( 'License Key', 'propertyhive' ),
-		);
-		return apply_filters( 'propertyhive_get_sections_' . $this->id, $sections );
-	}
-
-	public function get_pro_settings() {
-
-        $current_settings = get_option( 'propertyhive_pro', array() );
-
-        $settings = array(
-
-            array( 'title' => __( 'Active Features', 'propertyhive' ), 'type' => 'title', 'desc' => '', 'id' => 'pro_active_features_settings' )
-
-        );
-
-        $settings[] = array(
-            'type' => 'pro_features',
-        );
-
-        $settings[] = array( 'type' => 'sectionend', 'id' => 'pro_active_features_settings');
-
-        return $settings;
-    }
 
 	/**
 	 * Get settings array.
 	 *
 	 * @return array
 	 */
-	public function get_license_key_settings() {
+	public function get_settings() {
 
 		$license = PH()->license->get_current_license();
 
@@ -114,7 +80,7 @@ class PH_Settings_Licenses extends PH_Settings_Page {
 			}
 		}
 
-		$settings = apply_filters( 'propertyhive_licenses_settings', array(
+		$settings = array(
 
 			array( 'title' => __( 'License Key', 'propertyhive' ), 'type' => 'title', 'id' => 'license_options' ),
 
@@ -148,88 +114,13 @@ class PH_Settings_Licenses extends PH_Settings_Page {
 		return apply_filters( 'propertyhive_get_settings_' . $this->id, $settings );
 	}
 
-	public function pro_features_setting()
-    {
-        $features = get_ph_pro_features();
-
-        echo '<div class="pro-feature-settings">';
-
-        // filters
-
-        // list of features
-        echo '<ul>';
-        foreach ( $features as $feature )
-        {
-            $feature_status = false;
-
-            if ( is_dir( WP_PLUGIN_DIR . '/' . $feature['slug'] ) )
-            {
-                $feature_status = 'installed';
-            }
-            if ( is_plugin_active( $feature['plugin'] ) ) 
-            {
-                $feature_status = 'active';
-            }
-
-            echo '<li>
-                <div class="inner">
-                    <h3>' . ( ( isset($feature['icon']) && !empty($feature['icon']) ) ? '<span class="dashicons ' . $feature['icon'] . '"></span> ' : '' ) . $feature['name'] . '</h3>' . 
-                    ( ( isset($feature['pro']) && $feature['pro'] === true ) ? '<span class="pro">PRO</span>' : '' ) . 
-                    ( ( isset($feature['description']) && !empty($feature['description']) ) ? '<p>' . $feature['description'] . '</p>' : '' ) . '
-                    <p>
-                    ' . ( ( isset($feature['url']) && !empty($feature['url']) ) ? '<a href="' . $feature['url'] . '" target="_blank">' . __( 'Read More', 'propertyhive' ) . '</a>' : '' ) . 
-                    ( ( isset($feature['docs_url']) && !empty($feature['docs_url']) ) ? '&nbsp;&nbsp;|&nbsp;&nbsp;<a href="' . $feature['docs_url'] . '" target="_blank">' . __( 'Docs', 'propertyhive' ) . '</a>' : '' ) . 
-                    '</p>';
-
-                
-                if ( $feature_status == 'active' )
-                {
-                    $transient = get_site_transient( 'update_plugins' );
-                    if ( isset($transient->response) && is_array($transient->response) && isset($transient->response[$feature['plugin']]) && isset($transient->response[$feature['plugin']]->new_version) )
-                    {
-                        $plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . $feature['plugin'] );
-
-                        if ( version_compare($plugin_data['Version'], $transient->response[$feature['plugin']]->new_version, '<') )
-                        {
-                            echo '<div style="float:right"><a href="' . admin_url('update-core.php') . '" style="text-decoration:none"><span class="dashicons dashicons-update"></span> Update available</a></div>';
-                        }
-                    }
-                }
-
-                echo '<label class="switch">
-                  <input type="checkbox" name="active_plugins[]" value="' . $feature['slug'] . '"' . ( $feature_status == 'active' ? ' checked' : '' ) . '>
-                  <span class="slider round"></span>
-                </label>';
-
-                echo '<div class="loading"><img src="' . PH()->plugin_url() . '/assets/images/admin/loading.gif" alt=""></div>';
-
-                echo '</div>';
-            echo '</li>';
-        }
-        echo '</ul><div style="clear:both"></div>';
-
-        echo '</div>';
-    }
-
 	/**
 	 * Output the settings.
 	 */
 	public function output() {
 		global $current_section, $hide_save_button;
 
-		if ( $current_section ) 
-        {
-        	switch ($current_section)
-            {
-            	case "licensekey": { $settings = $this->get_license_key_settings(); break; }
-            	default: { die("Unknown setting section"); }
-            }
-        }
-        else
-        {
-        	$hide_save_button = true;
-        	$settings = $this->get_pro_settings(); 
-        }
+        $settings = $this->get_settings();
 
 		PH_Admin_Settings::output_fields( $settings );
 	}
@@ -239,31 +130,13 @@ class PH_Settings_Licenses extends PH_Settings_Page {
 	 */
 	public function save() {
 
-		if ( $current_section != '' ) 
-        {
-        	switch ($current_section)
-        	{
-        		case 'licensekey':
-				{
-					$settings = $this->get_license_key_settings();
+		$settings = $this->get_license_key_settings();
 
-					PH_Admin_Settings::save_fields( $settings );
+		PH_Admin_Settings::save_fields( $settings );
 
-					update_option( 'missing_invalid_expired_license_key_notice_dismissed', '' );
+		update_option( 'missing_invalid_expired_license_key_notice_dismissed', '' );
 
-					PH()->license->ph_check_licenses(true);
-					break;
-				}
-				default: { die("Unknown setting section"); }
-			}
-		}
-		else
-		{
-			//$settings = $this->get_settings();
-
-			//PH_Admin_Settings::save_fields( $settings );
-		}
-		
+		PH()->license->ph_check_licenses(true);
 	}
 }
 
