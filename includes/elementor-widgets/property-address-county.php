02c33ee21a25a33a8aced9053635d2bc91b99a68
Steve Marks <info@wp-property-hive.com>
2023-11-01 10:14:20 +0000

    Add individual address Elementor widgets

---
+++ includes/elementor-widgets/property-address-county.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Elementor Property Address County Widget.
+ *
+ * @since 1.0.0
+ */
+class Elementor_Property_Address_County_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'property-address-county';
+	}
+
+	public function get_title() {
+		return __( 'Address County', 'propertyhive' );
+	}
+
+	public function get_icon() {
+		return 'eicon-meta-data';
+	}
+
+	public function get_categories() {
+		return [ 'property-hive' ];
+	}
+
+	public function get_keywords() {
+		return [ 'property hive', 'propertyhive', 'property', 'address', 'county' ];
+	}
+
+	protected function register_controls() {
+
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => __( 'Address County', 'propertyhive' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'typography',
+				'label' => __( 'Typography', 'propertyhive' ),
+				'scheme' => \Elementor\Core\Schemes\Typography::TYPOGRAPHY_1,
+				'selector' => '{{WRAPPER}} .elementor-widget-address-county',
+			]
+		);
+
+		$this->add_control(
+			'color',
+			[
+				'label' => __( 'Colour', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => \Elementor\Core\Schemes\Color::get_type(),
+					'value' => \Elementor\Core\Schemes\Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-widget-address-county' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'text_align',
+			[
+				'label' => __( 'Alignment', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => __( 'Left', 'propertyhive' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'propertyhive' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', 'propertyhive' ),
+						'icon' => 'eicon-text-align-right',
+					],
+				],
+				'default' => 'left',
+				'toggle' => true,
+				'selectors' => [
+					'{{WRAPPER}} .elementor-widget-address-county' => 'text-align: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	protected function render() {
+
+		global $property;
+
+		$settings = $this->get_settings_for_display();
+
+		if ( !isset($property->id) ) {
+			return;
+		}
+
+		if ( $property->address_four != '' )
+		{
+	        echo '<div class="elementor-widget-address-county">';
+
+	        echo $property->address_four;
+
+	        echo '</div>';
+	    }
+
+	}
+
+}
---
+++ includes/elementor-widgets/property-address-street.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Elementor Property Address Street Widget.
+ *
+ * @since 1.0.0
+ */
+class Elementor_Property_Address_Street_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'property-address-street';
+	}
+
+	public function get_title() {
+		return __( 'Address Street', 'propertyhive' );
+	}
+
+	public function get_icon() {
+		return 'eicon-meta-data';
+	}
+
+	public function get_categories() {
+		return [ 'property-hive' ];
+	}
+
+	public function get_keywords() {
+		return [ 'property hive', 'propertyhive', 'property', 'address', 'street' ];
+	}
+
+	protected function register_controls() {
+
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => __( 'Address Street', 'propertyhive' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'typography',
+				'label' => __( 'Typography', 'propertyhive' ),
+				'scheme' => \Elementor\Core\Schemes\Typography::TYPOGRAPHY_1,
+				'selector' => '{{WRAPPER}} .elementor-widget-address-street',
+			]
+		);
+
+		$this->add_control(
+			'color',
+			[
+				'label' => __( 'Colour', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => \Elementor\Core\Schemes\Color::get_type(),
+					'value' => \Elementor\Core\Schemes\Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-widget-address-street' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'text_align',
+			[
+				'label' => __( 'Alignment', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => __( 'Left', 'propertyhive' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'propertyhive' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', 'propertyhive' ),
+						'icon' => 'eicon-text-align-right',
+					],
+				],
+				'default' => 'left',
+				'toggle' => true,
+				'selectors' => [
+					'{{WRAPPER}} .elementor-widget-address-street' => 'text-align: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	protected function render() {
+
+		global $property;
+
+		$settings = $this->get_settings_for_display();
+
+		if ( !isset($property->id) ) {
+			return;
+		}
+
+		if ( $property->address_street != '' )
+		{
+	        echo '<div class="elementor-widget-address-street">';
+
+	        echo $property->address_street;
+
+	        echo '</div>';
+	    }
+
+	}
+
+}
