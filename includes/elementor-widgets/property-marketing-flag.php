d96eccdd81448c557c52df4da3a236eb187b8c12
Steve Marks <info@wp-property-hive.com>
2024-03-20 10:28:35 +0000

    Add Elementor 'Marketing Flag' widget

---
+++ includes/elementor-widgets/property-marketing-flag.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Elementor Property Marketing Flag Widget.
+ *
+ * @since 1.0.0
+ */
+class Elementor_Property_Marketing_Flag_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'property-marketing-flag';
+	}
+
+	public function get_title() {
+		return __( 'Marketing Flag', 'propertyhive' );
+	}
+
+	public function get_icon() {
+		return 'eicon-meta-data';
+	}
+
+	public function get_categories() {
+		return [ 'property-hive' ];
+	}
+
+	public function get_keywords() {
+		return [ 'property hive', 'propertyhive', 'property', 'marketing flag', 'status' ];
+	}
+
+	protected function register_controls() {
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => __( 'Marketing Flag', 'propertyhive' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'typography',
+				'label' => __( 'Typography', 'propertyhive' ),
+				'scheme' => \Elementor\Core\Schemes\Typography::TYPOGRAPHY_1,
+				'selector' => '{{WRAPPER}} .elementor-widget-marketing-flag',
+			]
+		);
+
+		$this->add_control(
+			'color',
+			[
+				'label' => __( 'Colour', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => \Elementor\Core\Schemes\Color::get_type(),
+					'value' => \Elementor\Core\Schemes\Color::COLOR_1,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-widget-marketing-flag' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'text_align',
+			[
+				'label' => __( 'Alignment', 'propertyhive' ),
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => __( 'Left', 'propertyhive' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'propertyhive' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', 'propertyhive' ),
+						'icon' => 'eicon-text-align-right',
+					],
+				],
+				'default' => 'left',
+				'toggle' => true,
+				'selectors' => [
+					'{{WRAPPER}} .elementor-widget-marketing-flag' => 'text-align: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	protected function render() {
+
+		global $property;
+
+		$settings = $this->get_settings_for_display();
+
+		if ( !isset($property->id) ) {
+			return;
+		}
+
+		if ( $property->marketing_flag != '' )
+		{
+	        echo '<div class="elementor-widget-marketing-flag">' . $property->marketing_flag . '</div>';
+	    }
+
+	}
+
+}
