1a6c7208db0beb78bd30d002c71a8d99e3338a05
Steve Marks <info@wp-property-hive.com>
2024-05-27 16:39:15 +0100

    New actions in search form template

--- templates/global/search-form.php
+++ templates/global/search-form.php
@@ -12,12 +12,18 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 <form name="ph_property_search" class="property-search-form property-search-form-<?php echo esc_attr($id); ?> clear" action="<?php echo apply_filters( 'propertyhive_search_form_action', get_post_type_archive_link( 'property' ) ); ?>" method="get" role="form">
 
+    <?php do_action( 'propertyhive_before_search_form_controls', $id, $form_controls ); ?>
+
     <?php foreach ( $form_controls as $key => $field ) : ?>
 
         <?php ph_form_field( $key, $field ); ?>
 
     <?php endforeach; ?>
 
+    <?php do_action( 'propertyhive_after_search_form_controls', $id, $form_controls ); ?>
+
     <input type="submit" value="<?php echo esc_attr( 'Search', 'propertyhive' ); ?>">
+
+    <?php do_action( 'propertyhive_after_search_form_submit', $id, $form_controls ); ?>
 
 </form>
