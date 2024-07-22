96046c378bd74758fc2074f54e64064a724dd7bd
Steve Marks <info@wp-property-hive.com>
2024-07-22 07:25:26 +0100

    Fix department getting overwritten when 2 Elementor search forms are used

--- includes/elementor-widgets/property-search-form.php
+++ includes/elementor-widgets/property-search-form.php
@@ -381,12 +381,20 @@ class Elementor_Property_Search_Form_Widget extends \Elementor\Widget_Base {
 			</style>';
 		}
 
+		$original_department = isset($_GET['department']) ? $_GET['department'] : '';
+		$changed_department = false;
 		if ( isset($settings['default_department']) && !empty($settings['default_department']) && !isset($_GET['department']) )
 		{
 			$_GET['department'] = $settings['default_department'];
 			$_REQUEST['department'] = $settings['default_department'];
+
+			$changed_department = true;
 		}
 		ph_get_search_form( ( ( isset($settings['form_id']) && !empty($settings['form_id']) ) ? $settings['form_id'] : 'default' ) );
-
+		if ( $changed_department === true )
+		{
+			$_GET['department'] = $original_department;
+			$_REQUEST['department'] = $original_department;
+		}
 	}
 }
