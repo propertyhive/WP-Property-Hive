e82e713c40e73c8c71ac34cc44db25671d158c6b
Steve Marks <info@wp-property-hive.com>
2024-08-05 14:27:52 +0100

    Further tweaks to Flexslider loading issues

--- assets/js/flexslider/jquery.flexslider.init.js
+++ assets/js/flexslider/jquery.flexslider.init.js
@@ -1,88 +1,50 @@
 jQuery(window).on('load', function() {
     ph_init_slideshow();
+
+    // Ensure images are fully loaded before resizing
+    jQuery('#carousel.thumbnails .slides img').each(function() {
+        jQuery(this).on('load', function() {
+            setTimeout(function() { jQuery(window).trigger('resize'); }, 500);
+        });
+        // Trigger the load event for cached images
+        if (this.complete) jQuery(this).trigger('load');
+    });
+});
+
+jQuery(window).on('resize', function() {
+    // Set height of all thumbnails to be the same (i.e., height of the first one)
+    jQuery('#carousel.thumbnails .slides img').css('height', 'auto');
+    var thumbnail_height = jQuery('#carousel.thumbnails .slides img:eq(0)').height();
+    
+    // Apply the height to all thumbnails if it's greater than 0
+    if (thumbnail_height > 0) {
+        jQuery('#carousel.thumbnails .slides img').each(function() {
+            jQuery(this).height(thumbnail_height);
+        });
+    } else {
+        // Force redraw if height is 0
+        jQuery('#carousel.thumbnails .slides img').hide().show();
+    }
 });
 
 function ph_init_slideshow() {
-    // Loop through each instance of #slider using querySelectorAll
-    document.querySelectorAll('#slider').forEach(function(slider, index) {
-        var $slider = jQuery(slider);
+    // The slider being synced must be initialized first
+    jQuery('#carousel').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: true,
+        slideshow: false,
+        itemWidth: 150,
+        itemMargin: 5,
+        asNavFor: '#slider'
+    });
 
-        // Find the corresponding #carousel within the same parent container if it exists
-        var $parent = $slider.parent(); // Adjust this selector as needed based on your HTML structure
-        var $carousel = $parent.find('#carousel').length ? $parent.find('#carousel') : null;
-
-        if ($carousel && $carousel.length) {
-            // Initialize the carousel if it exists
-            $carousel.flexslider({
-                animation: "slide",
-                controlNav: false,
-                animationLoop: true,
-                slideshow: false,
-                itemWidth: 150,
-                itemMargin: 5,
-                asNavFor: $slider
-            });
-
-            // Initialize the slider
-            $slider.flexslider({
-                animation: "slide",
-                controlNav: false,
-                animationLoop: true,
-                slideshow: false,
-                sync: $carousel,
-                smoothHeight: true
-            });
-
-            // Ensure all images in the carousel are loaded before adjusting heights
-            var thumbnails = $carousel.find('.slides img');
-            var imagesLoadedCount = 0;
-            var totalImages = thumbnails.length;
-
-            function setThumbnailHeight() {
-                thumbnails.css('height', 'auto');
-                var thumbnail_height = thumbnails.eq(0).height();
-                // Check if the first thumbnail has a valid height
-                if (thumbnail_height > 0) 
-                {
-                    thumbnails.each(function() {
-                        jQuery(this).height(thumbnail_height);
-                    });
-                }
-                else
-                {
-                    setTimeout(setThumbnailHeight, 100); // Retry after a short delay
-                }
-            }
-
-            // Check if all images are loaded
-            function checkAllImagesLoaded() {
-                imagesLoadedCount++;
-                if (imagesLoadedCount === totalImages) {
-                    setThumbnailHeight();
-                    jQuery(window).on('resize', setThumbnailHeight);
-                }
-            }
-
-            // Attach load event to each image
-            thumbnails.each(function() {
-                if (this.complete) {
-                    // Image is already loaded
-                    checkAllImagesLoaded();
-                } else {
-                    // Attach event listener to load event
-                    jQuery(this).on('load', checkAllImagesLoaded);
-                }
-            });
-
-        } else {
-            // Initialize the slider without carousel sync
-            $slider.flexslider({
-                animation: "slide",
-                controlNav: false,
-                animationLoop: true,
-                slideshow: false,
-                smoothHeight: true
-            });
-        }
+    jQuery('#slider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: true,
+        slideshow: false,
+        sync: "#carousel",
+        smoothHeight: true
     });
 }
