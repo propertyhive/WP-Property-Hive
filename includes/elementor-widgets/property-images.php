049cf41021176ffffe4e185dc286b1aeea880c17
Steve Marks <info@wp-property-hive.com>
2024-09-26 12:05:25 +0100

    New optons in Elementor Property Images widget to specify click action

--- includes/elementor-widgets/property-images.php
+++ includes/elementor-widgets/property-images.php
@@ -53,8 +53,20 @@ class Elementor_Property_Images_Widget extends \Elementor\Widget_Base {
             [
                 'label' => __( 'Images To Show', 'propertyhive' ),
                 'type' => \Elementor\Controls_Manager::NUMBER,
-                //'default' => 1,
-                //'min' => 1,
+            ]
+        );
+
+        $this->add_control(
+            'link_to',
+            [
+                'label' => __( 'Link Images To', 'propertyhive' ),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'default' => '',
+				'options' => [
+					'' => esc_html__( 'Image In Lightbox', 'textdomain' ),
+					'blank' => esc_html__( 'Image In New Window', 'textdomain' ),
+					'property'  => esc_html__( 'Property URL', 'textdomain' ),
+				],
             ]
         );
 
@@ -84,6 +96,33 @@ class Elementor_Property_Images_Widget extends \Elementor\Widget_Base {
 			remove_action( 'propertyhive_product_thumbnails', 'propertyhive_show_property_thumbnails', 20 );
 		}
 
+		if ( isset($settings['link_to']) )
+		{
+			switch ($settings['link_to'])
+			{
+				case "blank":
+				{
+					add_filter( 'propertyhive_single_property_image_html', array( $this, 'customise_property_images_html_blank' ), 10, 2 );
+					break;
+				}
+				case "property":
+				{
+					add_filter( 'propertyhive_single_property_image_html', array( $this, 'customise_property_images_html_property' ), 10, 2 );
+					break;
+				}
+				default:
+				{
+					wp_enqueue_script( 'propertyhive_fancybox' );
+					wp_enqueue_style( 'propertyhive_fancybox_css' );
+				}
+			}
+		}
+		else
+		{
+			wp_enqueue_script( 'propertyhive_fancybox' );
+			wp_enqueue_style( 'propertyhive_fancybox_css' );
+		}
+
 		propertyhive_show_property_images( ( isset($settings['num_images']) && !empty($settings['num_images']) && is_numeric($settings['num_images']) ) ? (int)$settings['num_images'] : '' );
 
 		// On render widget from Editor - trigger the init manually.
@@ -98,4 +137,18 @@ class Elementor_Property_Images_Widget extends \Elementor\Widget_Base {
 
 	}
 
+	public function customise_property_images_html_blank( $html, $post_id )
+	{
+		$html = str_replace("data-fancybox=\"gallery-" . $post_id . "\"", "target=\"_blank\"", $html);
+		return $html;
+	}
+
+	public function customise_property_images_html_property( $html, $post_id )
+	{
+		$html = str_replace("data-fancybox=\"gallery-" . $post_id . "\"", "", $html);
+
+		$html = preg_replace('/(href=")([^"]*)(")/', 'href="' . get_permalink($post_id) . '"', $html);
+
+		return $html;
+	}
 }
