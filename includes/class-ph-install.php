044afb1ddf62e275cceb60a50da6ec83f3850385
Steve Marks <info@wp-property-hive.com>
2025-11-11 15:22:57 +0000

    Create search page and assign by default when installing

--- includes/class-ph-install.php
+++ includes/class-ph-install.php
@@ -96,6 +96,7 @@ class PH_Install {
         if ( is_null( $current_version ) && is_null( $current_db_version ) ) 
         {
             $this->create_terms();
+            $this->create_pages();
             set_transient( '_ph_activation_redirect', 1, 30 );
         }
         
@@ -212,7 +213,7 @@ class PH_Install {
 	 * @access public
 	 * @return void
 	 */
-	public static function create_pages() {
+	public function create_pages() {
 
         // Create page object
         $my_post = array(
@@ -225,9 +226,12 @@ class PH_Install {
         );
         
         // Insert the post into the database
-        $page_id = wp_insert_post( $my_post );
+        $page_id = wp_insert_post( $my_post, true );
         
-        update_option( 'propertyhive_search_results_page_id', $page_id );
+        if ( !is_wp_error($page_id) )
+        {
+            update_option( 'propertyhive_search_results_page_id', $page_id );
+        }
 	}
 
 	/**
