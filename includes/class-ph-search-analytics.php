97a509ca34d42b50325aa5f310def41d4d35edc4
Steve Marks <info@wp-property-hive.com>
2026-07-29 14:29:03 +0100

    Exclude search logging for admin users

--- includes/class-ph-search-analytics.php
+++ includes/class-ph-search-analytics.php
@@ -33,6 +33,11 @@ class PH_Search_Analytics
 			return;
 		}
 
+		if ( current_user_can( 'manage_propertyhive' ) ) 
+		{
+		    return;
+		}
+
 		if ( 
 			!is_post_type_archive( 'property' ) && 
 			!is_page( ph_get_page_id( 'search_results' ) ) 
